422df25dd53bce0f6d92011d8ca3b3e1e7abea9d
lasalesi <larsericmueller@gmail.com>
2022-03-17 14:32:26 +0100

    basic auth

--- microsynth/microsynth/webshop.py
+++ microsynth/microsynth/webshop.py
@@ -10,22 +10,12 @@ from microsynth.microsynth.migration import update_customer
 def ping():
     return "pong"
     
-@frappe.whitelist(allow_guest=True)
-def create_update_customer(key, customer_data):
-    if check_key(key):
-        if type(customer_data) == str:
-            customer_data = json.loads(customer_data)
-        error = update_customer(customer_data)
-        if not error:
-            return {'status': 'Success'}
-        else: 
-            return {'status': error}
-    else:
-        return {'status': 'Authentication failed'}
-
-def check_key(key):
-    server_key = frappe.get_value("Microsynth Webshop Settings", "Microsynth Webshop Settings", "preshared_key")
-    if server_key == key:
-        return True
-    else:
-        return False
+@frappe.whitelist()
+def create_update_customer(customer_data):
+    if type(customer_data) == str:
+        customer_data = json.loads(customer_data)
+    error = update_customer(customer_data)
+    if not error:
+        return {'status': 'Success'}
+    else: 
+        return {'status': error}
