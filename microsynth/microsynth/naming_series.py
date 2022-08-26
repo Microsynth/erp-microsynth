507e29612d323ea758017a673ee5a910d31dfc5c
lasalesi <larsericmueller@gmail.com>
2022-08-26 10:28:58 +0200

    additional naming series

--- microsynth/microsynth/naming_series.py
+++ microsynth/microsynth/naming_series.py
@@ -5,12 +5,40 @@
 import frappe
 
 NAMING_SERIES_MAP = {
+    'Standing Quotation': {
+		'Microsynth AG': 'SQ-.YY.#####',
+		'Microsynth Seqlab GmbH': 'SQ-.YY.#####',
+		'Microsynth Austria GmbH': 'SQ-.YY.#####',
+		'Microsynth France SAS': 'SQ-.YY.#####',
+		'Ecogenics GmbH': 'SQ-.YY.#####'
+	},
+	'Quotation': {
+		'Microsynth AG': 'QTN-.YY.#####',
+		'Microsynth Seqlab GmbH': 'QTN-.YY.#####',
+		'Microsynth Austria GmbH': 'QTN-.YY.#####',
+		'Microsynth France SAS': 'QTN-.YY.#####',
+		'Ecogenics GmbH': 'QTN-.YY.#####'
+	},
     'Sales Order': {
 		'Microsynth AG': 'SO-BAL-.YY.######',
 		'Microsynth Seqlab GmbH': 'SO-GOE-.YY.######',
 		'Microsynth Austria GmbH': 'SO-WIE-.YY.######',
 		'Microsynth France SAS': 'SO-LYO-.YY.######',
 		'Ecogenics GmbH': 'SO-ECO-.YY.######'
+	},
+    'Delivery Note': {
+		'Microsynth AG': 'DN-BAL-.YY.######',
+		'Microsynth Seqlab GmbH': 'DN-GOE-.YY.######',
+		'Microsynth Austria GmbH': 'DN-WIE-.YY.######',
+		'Microsynth France SAS': 'DN-LYO-.YY.######',
+		'Ecogenics GmbH': 'DN-ECO-.YY.######'
+	},
+    'Sales Invoice': {
+		'Microsynth AG': 'SI-BAL-.YY.######',
+		'Microsynth Seqlab GmbH': 'SI-GOE-.YY.######',
+		'Microsynth Austria GmbH': 'SI-WIE-.YY.######',
+		'Microsynth France SAS': 'SI-LYO-.YY.######',
+		'Ecogenics GmbH': 'SI-ECO-.YY.######'
 	}
 }
 
