60965907aa9d807c0d909527333504b0a510e2c0
lasalesi <larsericmueller@gmail.com>
2022-09-20 14:30:48 +0200

    new pick queue/wizard

--- microsynth/microsynth/report/open_label_orders/open_label_orders.js
+++ microsynth/microsynth/report/open_label_orders/open_label_orders.js
@@ -8,73 +8,168 @@ frappe.query_reports["Open Label Orders"] = {
     ],
     "onload": (report) => {
         report.page.add_inner_button( __("Pick labels"), function() {
-            pick_wizard();
+            queue_builder();
         });
     }
 };
 
-function pick_wizard() {
-    // get all orders to process
+function queue_builder() {
     frappe.call({
         'method': "microsynth.microsynth.report.open_label_orders.open_label_orders.get_data",
-        'callback': function(r)
-        {
-            // show scan dialog
-            for (var i = 0; i < r.message.length; i++) {
-                frappe.prompt([
-                    {
-                        'fieldname': 'sales_order', 
-                        'fieldtype': 'Link', 
-                        'label': 'Sales Order', 
-                        'read_only': 1, 
-                        'options': 'Sales Order', 
-                        'default': r.message[i].sales_order
-                    },
-                    {
-                        'fieldname': 'item', 
-                        'fieldtype': 'Data', 
-                        'label': __('Item'), 
-                        'read_only': 1, 
-                        'default': r.message[i].qty + "x " + r.message[i].item_code + ": " + r.message[i].item_name
-                    },
-                    {
-                        'fieldname': 'range', 
-                        'fieldtype': 'Data', 
-                        'label': 'Range', 
-                        'read_only': 1, 
-                        'default': r.message[i].range
-                    },
-                    {
-                        'fieldname': 'from_barcode', 
-                        'fieldtype': 'Data', 
-                        'label': 'From barcode'
-                    },
-                    {
-                        'fieldname': 'to_barcode', 
-                        'fieldtype': 'Data', 
-                        'label': 'To barcode'
-                    }
-                ],
-                function (values) {
-                    // create delivery note / pdf
-                    frappe.call({
-                        'method': "microsynth.microsynth.report.open_label_orders.open_label_orders.pick_labels",
-                        'args': {
-                            'sales_order': values.sales_order,
-                            'from_barcode': values.from_barcode,
-                            'to_barcode': values.to_barcode
-                        },
-                        'callback': function(r2) {
-                            // open print dialog & print
-                            console.log(r2.message);
-                            window.open("/printview?doctype=Delivery%20Note&name=" + r2.message + "&trigger_print=1&format=Standard&no_letterhead=0&_lang=en", '_blank').focus();
-                        }
-                    });
-                },
-                __("Pick labels"),
-                __("OK")
-                )
+        'callback': function(r) {
+            // reset queue
+            locals.label_queue = r.message;
+            // add status flags to each entry
+            for (var i = 0; i < locals.label_queue.length; i++) {
+                locals.label_queue[i].status = 0;
             }
+            // start queue processing
+            process_queue()
         }
     });
 }
+
+function process_queue() {
+    if (locals.label_queue.length > 0) {
+        // process first entry
+        label_order = locals.label_queue[0];
+        if (label_order.status === 0) {
+            // nothing done - first barcode
+            first_barcode_dialog();
+        } else if (label_order.status === 1) {
+            // first barcode done - get second barcode
+            second_barcode_dialog();
+        } else {
+            // create delivery note / pdf
+                frappe.call({
+                    'method': "microsynth.microsynth.report.open_label_orders.open_label_orders.pick_labels",
+                    'args': {
+                        'sales_order': locals.label_queue[0].sales_order,
+                        'from_barcode': locals.label_queue[0].from_barcode,
+                        'to_barcode': locals.label_queue[0].to_barcode
+                    },
+                    'callback': function(r) {
+                        // open print dialog & print
+                        console.log(r.message);
+                        window.open("/printview?doctype=Delivery%20Note&name=" + r.message + "&trigger_print=1&format=Standard&no_letterhead=0&_lang=en", '_blank').focus();
+                    }
+                });
+            // kick first order out and resume
+            locals.label_queue.shift();
+            process_queue();
+        }
+    }
+}
+
+
+function first_barcode_dialog() {
+    if (locals.label_queue.length > 0) {
+        // show scan first dialog
+        frappe.prompt(
+            [
+                {
+                    'fieldname': 'from_barcode', 
+                    'fieldtype': 'Data', 
+                    'label': 'From barcode'
+                },
+                {
+                    'fieldname': 'sales_order', 
+                    'fieldtype': 'Link', 
+                    'label': 'Sales Order', 
+                    'read_only': 1, 
+                    'options': 'Sales Order', 
+                    'default': locals.label_queue[0].sales_order
+                },
+                {
+                    'fieldname': 'item', 
+                    'fieldtype': 'Data', 
+                    'label': __('Item'), 
+                    'read_only': 1, 
+                    'default': locals.label_queue[0].qty + "x " + locals.label_queue[0].item_code + ": " + locals.label_queue[0].item_name
+                },
+                {
+                    'fieldname': 'range', 
+                    'fieldtype': 'Data', 
+                    'label': 'Range', 
+                    'read_only': 1, 
+                    'default': locals.label_queue[0].range
+                }
+            ],
+            function (values) {
+                // validation: if it fails, leave status on 0 and continue, on success move to 1
+                var validated = false;
+                
+                // TODO
+                validated = true;
+                
+                if (validated) {
+                    locals.label_queue[0].status = 1;
+                }
+                locals.label_queue[0].from_barcode = values.from_barcode;
+                process_queue();
+            },
+            __("Pick first label"),
+            __("OK")
+        )
+    }
+}
+
+function second_barcode_dialog() {
+    if (locals.label_queue.length > 0) {
+        // show scan second dialog
+        frappe.prompt(
+            [
+                {
+                    'fieldname': 'to_barcode', 
+                    'fieldtype': 'Data', 
+                    'label': 'To barcode'
+                },
+                {
+                    'fieldname': 'sales_order', 
+                    'fieldtype': 'Link', 
+                    'label': 'Sales Order', 
+                    'read_only': 1, 
+                    'options': 'Sales Order', 
+                    'default': locals.label_queue[0].sales_order
+                },
+                {
+                    'fieldname': 'item', 
+                    'fieldtype': 'Data', 
+                    'label': __('Item'), 
+                    'read_only': 1, 
+                    'default': locals.label_queue[0].qty + "x " + locals.label_queue[0].item_code + ": " + locals.label_queue[0].item_name
+                },
+                {
+                    'fieldname': 'range', 
+                    'fieldtype': 'Data', 
+                    'label': 'Range', 
+                    'read_only': 1, 
+                    'default': locals.label_queue[0].range
+                },
+                {
+                    'fieldname': 'from_barcode', 
+                    'fieldtype': 'Data', 
+                    'label': 'From barcode', 
+                    'read_only': 1, 
+                    'default': locals.label_queue[0].from_barcode
+                }
+            ],
+            function (values) {
+                // validation: if it fails, leave status on 0 and continue, on success move to 1
+                var validated = false;
+                
+                // TODO
+                validated = true;
+                
+                if (validated) {
+                    locals.label_queue[0].status = 2;
+                }
+                locals.label_queue[0].to_barcode = values.to_barcode;
+                process_queue();
+            },
+            __("Pick last label"),
+            __("OK")
+        )
+    }
+}
+
