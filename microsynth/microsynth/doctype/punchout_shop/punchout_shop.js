b531d438d854461f1daef4e1ee4e7caaae4ca561
Rolf Suter <rolf.suter@gmx.net>
2022-10-25 11:25:14 +0200

    Add Punchout Shop DocType

---
+++ microsynth/microsynth/doctype/punchout_shop/punchout_shop.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Microsynth, libracore and contributors and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Punchout Shop', {
+	// refresh: function(frm) {
+
+	// }
+});
