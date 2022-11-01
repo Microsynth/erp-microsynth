9d2686f716e7f55cb2d53bdcab0a69c5160a3838
Rolf Suter <rolf.suter@gmx.net>
2022-11-01 15:03:11 +0100

    Set item price by defining a discount on the reference

--- microsynth/microsynth/report/pricing_configurator/pricing_configurator.js
+++ microsynth/microsynth/report/pricing_configurator/pricing_configurator.js
@@ -59,6 +59,26 @@ frappe.query_reports["Pricing Configurator"] = {
                 var item_code = frappe.query_report.data[row].item_code;
                 edit_cell(item_code, price_list, qty, value);
             }
+            else if (parseInt(column) === 8 ) {
+                // fetch value
+                var discount = 1;
+                if ((event.delegatedTarget.innerText) && (event.delegatedTarget.innerText.length > 0)) {
+                    discount = parseFloat(event.delegatedTarget.innerText);
+                }
+                // get price list fom filters
+                var price_list = null;
+                for (var i = 0; i < frappe.query_report.filters.length; i++) {
+                    if (frappe.query_report.filters[i].fieldname == "price_list") {
+                        price_list = frappe.query_report.filters[i].value;
+                    }
+                }
+                // get the quantity from data
+                var qty = frappe.query_report.data[row].qty
+                // get item_code from data
+                var item_code = frappe.query_report.data[row].item_code;
+                var reference_rate = frappe.query_report.data[row].reference_rate;
+                edit_discount_cell(item_code, price_list, qty, discount, reference_rate);
+            }
         });
     }
 };
@@ -119,6 +139,39 @@ function edit_cell(item_code, price_list, qty, value) {
                     'price_list': values.price_list,
                     'qty': values.qty,
                     'rate': values.rate
+                },
+                'callback': function(r)
+                {
+                    frappe.query_report.refresh();
+                }
+            });
+        },
+        'primary_action_label': __('Change'),
+        'title': __('Change rate')
+    });
+    d.show();
+}
+
+function edit_discount_cell(item_code, price_list, qty, discount, reference_rate) {
+    var d = new frappe.ui.Dialog({
+        'fields': [
+            {'fieldname': 'price_list', 'fieldtype': 'Link', 'options': "Price List", 'label': __('Price List'), 'read_only': 1, 'default': price_list},
+            {'fieldname': 'item_code', 'fieldtype': 'Link', 'options': "Item", 'label': __('Item'), 'read_only': 1, 'default': item_code},
+            {'fieldname': 'qty', 'fieldtype': 'Float', 'precision': 2, 'label': __('Quantity'), 'read_only': 1, 'default': qty},
+            {'fieldname': 'rate', 'fieldtype': 'Float', 'precision': 2, 'label': __('Reference Rate'), 'read_only': 1, 'default': reference_rate},
+            {'fieldname': 'discount', 'fieldtype': 'Float', 'precision': 2, 'label': __('Discount'), 'reqd': 1, 'default': discount}
+        ],
+        'primary_action': function(){
+            d.hide();
+            var values = d.get_values();
+            var rate = reference_rate  * (1 - values.discount/100)
+            frappe.call({
+                'method': "microsynth.microsynth.report.pricing_configurator.pricing_configurator.set_rate",
+                'args':{
+                    'item_code': values.item_code,
+                    'price_list': values.price_list,
+                    'qty': values.qty,
+                    'rate': rate
                 },
                 'callback': function(r)
                 {
