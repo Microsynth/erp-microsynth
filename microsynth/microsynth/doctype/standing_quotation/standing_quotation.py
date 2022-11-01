39ff809690a6c43ef952f572e42c63ff7de98aa9
Rolf Suter <rolf.suter@gmx.net>
2022-11-01 16:01:30 +0100

    Attach pdf to standing quotation when submitting

--- microsynth/microsynth/doctype/standing_quotation/standing_quotation.py
+++ microsynth/microsynth/doctype/standing_quotation/standing_quotation.py
@@ -3,8 +3,11 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
+from erpnextswiss.erpnextswiss.attach_pdf import attach_pdf
 from frappe.model.document import Document
 
 class StandingQuotation(Document):
-	pass
+	def on_submit(self):
+		attach_pdf(doctype='Standing Quotation', docname=self.name, print_format='Standing Quotation')		
+		return
