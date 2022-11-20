bfd1d23d97d53b9c27a6c43f6f561676aface77d
Rolf Suter <rolf.suter@gmx.net>
2022-11-20 10:19:24 +0100

    Draft Address Finder filters

--- microsynth/microsynth/report/address_finder/address_finder.js
+++ microsynth/microsynth/report/address_finder/address_finder.js
@@ -4,6 +4,62 @@
 
 frappe.query_reports["Address Finder"] = {
 	"filters": [
+		{
+			"fieldname": "customer",
+			"label": __("Customer"),
+			"fieldtype": "Data",
+			"options": ""
 
+		}, 
+		{
+			"fieldname": "contact_full_name",
+			"label": __("Contact name"),
+			"fieldtype": "Data",
+			"options": ""
+		},
+		{
+			"fieldname": "contact_email",
+			"label": __("Contact Email"),
+			"fieldtype": "Data",
+			"options": ""
+		},
+		,
+		{
+			"fieldname": "contact_institute",
+			"label": __("Institute"),
+			"fieldtype": "Data",
+			"options": ""
+		},  
+		,
+		{
+			"fieldname": "contact_departement",
+			"label": __("Departement"),
+			"fieldtype": "Data",
+			"options": ""
+		},  
+		{
+			"fieldname": "contact_group_leader",
+			"label": __("Group Leader"),
+			"fieldtype": "Data",
+			"options": ""
+		},
+		{
+			"fieldname": "contact_intitute_key",
+			"label": __("Institute Key"),
+			"fieldtype": "Data",
+			"options": ""
+		},
+		{
+			"fieldname": "address_city",
+			"label": __("City"),
+			"fieldtype": "Data",
+			"options": ""
+		},
+		{
+			"fieldname": "address_street",
+			"label": __("Street"),
+			"fieldtype": "Data",
+			"options": ""
+		}
 	]
 };
