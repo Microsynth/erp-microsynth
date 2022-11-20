ad3baa7b7e4ec39c36868c4b63bf19f081459b7a
Rolf Suter <rolf.suter@gmx.net>
2022-11-20 10:52:44 +0100

    Draft columns

--- microsynth/microsynth/report/address_finder/address_finder.py
+++ microsynth/microsynth/report/address_finder/address_finder.py
@@ -1,9 +1,42 @@
-# Copyright (c) 2013, Microsynth, libracore and contributors and contributors
+# Copyright (c) 2022, Microsynth, libracore and contributors and contributors
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
+from frappe import _
 
 def execute(filters=None):
-	columns, data = [], []
+	columns, data = get_columns(filters), get_data(filters)
 	return columns, data
+
+def get_columns(filters):
+	return [		
+		{"label": _("Customer"), "fieldname": "customer", "fieldtype": "Data", "width": 75},
+		{"label": _("First Name"), "fieldname": "first_name", "fieldtype": "Data", "width": 60},
+		{"label": _("Last Name"), "fieldname": "last_name", "fieldtype": "Data", "width": 60},
+		{"label": _("Institute"), "fieldname": "institute", "fieldtype": "Data", "width": 75},
+		{"label": _("Departement"), "fieldname": "departement", "fieldtype": "Data", "width": 75},
+		{"label": _("Group Leader"), "fieldname": "group_leader", "fieldtype": "Data", "width": 50},
+		{"label": _("Institute key"), "fieldname": "institute_key", "fieldtype": "Data", "width": 50},
+		{"label": _("City"), "fieldname": "city", "fieldtype": "Data", "width": 50},
+		{"label": _("Street"), "fieldname": "street", "fieldtype": "Data", "width": 50},
+	]
+
+def get_data(filters):
+	data = []
+
+	entry = {
+		"customer": "ETHZ",
+		"first_name":"Peter",
+		"last_name":"Musterkerl",
+		"institute":"Biochemistry",
+		"departement":"Chemistry & Biology",
+		"group_leader":"Müller",
+		"institute_key":"CH_01_01",
+		"city":"Zürich",
+		"street":"Langstrasse"
+	}
+
+	data.append(entry)
+
+	return data
