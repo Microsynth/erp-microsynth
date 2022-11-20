17862b92acf9f0cfbe27e7336f0d0480206e2627
Rolf Suter <rolf.suter@gmx.net>
2022-11-20 10:07:41 +0100

    Add Address Finder report

---
+++ microsynth/microsynth/report/address_finder/address_finder.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, Microsynth, libracore and contributors and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Address Finder"] = {
+	"filters": [
+
+	]
+};
