bf190a0874be4966b2eb860f5b7c0495ce0c77a5
Rolf Suter <rolf.suter@gmx.net>
2022-11-20 18:08:38 +0100

    Copy code from 'Address Finder' to 'Customer Finder' for renaming

--- microsynth/microsynth/report/customer_finder/customer_finder.js
+++ microsynth/microsynth/report/customer_finder/customer_finder.js
@@ -1,9 +1,62 @@
-// Copyright (c) 2016, Microsynth, libracore and contributors and contributors
+// Copyright (c) 2022, Microsynth, libracore and contributors and contributors
 // For license information, please see license.txt
 /* eslint-disable */
 
 frappe.query_reports["Customer Finder"] = {
 	"filters": [
-
+		{
+			"fieldname": "customer",
+			"label": __("Customer"),
+			"fieldtype": "Data",
+			"options": ""
+		}, 
+		{
+			"fieldname": "contact_full_name",
+			"label": __("Contact name"),
+			"fieldtype": "Data",
+			"options": ""
+		},
+		{
+			"fieldname": "contact_email",
+			"label": __("Contact Email"),
+			"fieldtype": "Data",
+			"options": ""
+		},
+		{
+			"fieldname": "contact_institute",
+			"label": __("Institute"),
+			"fieldtype": "Data",
+			"options": ""
+		},  
+		{
+			"fieldname": "contact_department",
+			"label": __("Department"),
+			"fieldtype": "Data",
+			"options": ""
+		},  
+		{
+			"fieldname": "contact_group_leader",
+			"label": __("Group Leader"),
+			"fieldtype": "Data",
+			"options": ""
+		},
+		{
+			"fieldname": "contact_institute_key",
+			"label": __("Institute Key"),
+			"fieldtype": "Data",
+			"options": ""
+		},
+		{
+			"fieldname": "address_city",
+			"label": __("City"),
+			"fieldtype": "Data",
+			"options": ""
+		},
+		{
+			"fieldname": "address_street",
+			"label": __("Street"),
+			"fieldtype": "Data",
+			"options": ""
+		}
 	]
 };
