79c18912148306ec8bc5681cc2166926f9fce481
Joerg Vogt <Joerg.Vogt@microsynth.ch>
2022-12-14 15:16:01 +0100

    button or printing delivery labels in sales_order form

--- microsynth/public/js/sales_order.js
+++ microsynth/public/js/sales_order.js
@@ -13,3 +13,18 @@ frappe.ui.form.on('Sales Order Item', {
         fetch_price_list_rate(frm, cdt, cdn);
     }
 });
+
+frappe.ui.form.on('Sales Order', {
+    refresh(frm) {
+        if (!frm.doc.__islocal) {
+            frm.add_custom_button(__("Print Delivery Label"), function() {
+                frappe.call({
+                    "method": "microsynth.microsynth.labels.print_address_template",
+                    "args": {
+                        "sales_order_id": frm.doc.name
+                    }
+                })
+            });
+        }
+    }
+});
