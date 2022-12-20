5584bb7967da9ef0126f4806a5b00919c92c544a
Rolf Suter <rolf.suter@gmx.net>
2022-12-20 10:23:20 +0100

    Query data for Oligo Orders to Export

--- microsynth/microsynth/report/oligo_orders_export/oligo_orders_export.py
+++ microsynth/microsynth/report/oligo_orders_export/oligo_orders_export.py
@@ -2,8 +2,47 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
+from frappe import _
 
 def execute(filters=None):
-	columns, data = [], []
+	columns = get_columns()
+	data = get_data(filters)
 	return columns, data
+
+def get_columns():
+	return [
+        {"label": _("Sales Order"), "fieldname": "sales_order", "fieldtype": "Link", "options": "Sales Order", "width": 120},
+        {"label": _("Customer"), "fieldname": "customer", "fieldtype": "Link", "options": "Customer", "width": 70},
+        {"label": _("Customer name"), "fieldname": "customer_name", "fieldtype": "Data", "width": 180},
+        {"label": _("Contact"), "fieldname": "contact", "fieldtype": "Link", "options": "Contact", "width": 60},
+        {"label": _("Contact name"), "fieldname": "contact_name", "fieldtype": "Data", "width": 180},
+        {"label": _("Date"), "fieldname": "date", "fieldtype": "Date", "width": 80},
+        {"label": _("Code"), "fieldname": "export_code", "fieldtype": "Data", "width": 60},
+		{"label": _("Comment"), "fieldname": "comment", "fieldtype": "Data", "width": 100}
+	]
+
+@frappe.whitelist()
+def get_data(filters=None):
+    open_oligo_orders = frappe.db.sql("""
+        SELECT 
+            `tabSales Order`.`name` AS `sales_order`,
+            `tabSales Order`.`customer` AS `customer`,
+            `tabSales Order`.`customer_name` AS `customer_name`,
+            `tabSales Order`.`contact_person` AS `contact`,
+            `tabSales Order`.`contact_display` AS `contact_name`,
+            `tabSales Order`.`transaction_date` AS `date`,
+			`tabCountry`.`export_code` AS `export_code`,
+            `tabSales Order`.`comment` AS `comment`
+        FROM `tabSales Order`
+        LEFT JOIN `tabAddress` ON `tabAddress`.`name` = `tabSales Order`.`shipping_address_name`
+        LEFT JOIN `tabCountry` ON `tabCountry`.`name` = `tabAddress`.`country`
+        WHERE 
+            `tabSales Order`.`product_type` = "Oligos"
+            AND `tabSales Order`.`docstatus` = 1
+            AND `tabCountry`.`name` <> 'Switzerland'
+			AND `tabSales Order`.`transaction_date` >= DATE_SUB(NOW(), INTERVAL 1 YEAR)
+        ORDER BY `tabSales Order`.`transaction_date` ASC;
+    """, as_dict=True)
+    
+    return open_oligo_orders
