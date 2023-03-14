d39719915722c90ae5bdb4e2b229ffd600a7ab39
lasalesi <larsericmueller@gmail.com>
2023-03-14 16:32:19 +0100

    clone button

--- microsynth/public/js/sales_invoice.js
+++ microsynth/public/js/sales_invoice.js
@@ -1,10 +1,16 @@
 /* Custom script extension for Sales Invoice */
 frappe.ui.form.on('Sales Invoice', {
     refresh(frm) {
-		prepare_naming_series(frm);				// common function
-	},
+        prepare_naming_series(frm);				// common function
+        
+        if (frm.doc.docstatus > 0) {
+            frm.add_custom_button(__("Clone"), function() {
+                clone(frm);
+            });
+        }
+    },
     company(frm) {
-		set_naming_series(frm);					// common function
+        set_naming_series(frm);					// common function
     }
 });
 
@@ -13,3 +19,16 @@ frappe.ui.form.on('Sales Invoice Item', {
         fetch_price_list_rate(frm, cdt, cdn);
     }
 });
+
+function clone(frm) {
+    frappe.call({
+        'method': "microsynth.microsynth.utils.exact_copy_sales_invoice",
+        'args': {
+            'sales_invoice': frm.doc.name
+        },
+        'callback': function(r)
+        {
+            frappe.set_route("Form", "Sales Invoice", r.message)
+        }
+    });
+}
