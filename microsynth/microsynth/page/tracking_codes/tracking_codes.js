6dbcf66bbfdf1460dd4bb6ca6e2cfc739bd864d1
Rolf Suter <rolf.suter@gmx.net>
2023-03-28 15:43:33 +0200

    Sketch the Tracking Codes page

--- microsynth/microsynth/page/tracking_codes/tracking_codes.js
+++ microsynth/microsynth/page/tracking_codes/tracking_codes.js
@@ -1,7 +1,54 @@
 frappe.pages['tracking_codes'].on_page_load = function(wrapper) {
-	var page = frappe.ui.make_app_page({
-		parent: wrapper,
-		title: 'Tracking Codes',
-		single_column: true
-	});
+    var page = frappe.ui.make_app_page({
+        parent: wrapper,
+        title: 'Tracking Codes',
+        single_column: true
+    });
+    frappe.tracking_codes.make(page);
+    frappe.tracking_codes.run();
+}
+
+frappe.tracking_codes = {
+    start: 0,
+    make: function(page) {
+        var me = frappe.tracking_codes;
+        me.page = page;
+        me.body = $('<div></div>').appendTo(me.page.main);
+        var data = "";
+        $(frappe.render_template('tracking_codes', data)).appendTo(me.body);
+    },
+    run: function() {       
+        // add on enter listener to QR code box
+        document.getElementById("work_order").addEventListener("keyup", function(event) {
+            event.preventDefault();
+            if (event.keyCode === 13) {
+                if (this.value.startsWith("MA-")) {
+                    // this is an employee key
+                    var employee = document.getElementById("employee");
+                    employee.value = this.value;
+                    this.value = "";
+                    // reload work order if one is open
+                    var work_order = document.getElementById("work_order_reference").value;
+                    if (work_order) {
+                        frappe.production_control.launch(work_order);
+                    }
+                } else {
+                    frappe.production_control.launch(this.value);
+                    this.value = "";
+                }
+            }
+        });
+        // check for url parameters
+        var url = location.href;
+        if (url.indexOf("?work_order=") >= 0) {
+            var work_order = url.split('=')[1].split('&')[0];
+            document.getElementById("work_order").value = work_order;
+            frappe.production_control.get_work_order(work_order);
+        }
+    }
+    
+}
+
+function clear_qr() {
+    document.getElementById("work_order").value = "";
 }
