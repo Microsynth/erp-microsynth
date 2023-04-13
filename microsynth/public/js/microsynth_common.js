6f93491340ebfb50595e6c9834c531f9d90fc671
lasalesi <larsericmueller@gmail.com>
2023-04-13 11:13:52 +0200

    moved avis wizard to common functions

--- microsynth/public/js/microsynth_common.js
+++ microsynth/public/js/microsynth_common.js
@@ -84,3 +84,170 @@ function update_taxes(company, customer, address, category) {
     });
 }
 
+/// Avis booking
+function allocate_avis(frm) {
+    var d = new frappe.ui.Dialog({
+        'fields': [
+            {
+                'fieldname': 'camt_amount', 
+                'fieldtype': 'Float', 
+                'label': __('Amount'), 
+                'default': frm.doc.camt_amount,
+                'precision': 2,
+                'read_only': 1
+            },
+            {
+                'fieldname': 'col_1', 
+                'fieldtype': 'Column Break'
+            },
+            {
+                'fieldname': 'currency', 
+                'fieldtype': 'Data', 
+                'label': __('Currency'), 
+                'default': frm.doc.paid_to_account_currency,
+                'read_only': 1
+            },
+            {
+                'fieldname': 'sec_1', 
+                'fieldtype': 'Section Break', 
+                'label': __('Allocation')
+            },
+            {
+                'fieldname': 'invoices', 
+                'fieldtype': 'Table', 
+                'label': __('Invoices'), 
+                'reqd': 1,
+                'fields' : [
+                    {
+                        'fieldname': 'sales_invoice', 
+                        'fieldtype': 'Link', 
+                        'label': __('Sales Invoice'),
+                        'options': "Sales Invoice",
+                        'in_list_view': 1,
+                        'reqd': 1,
+                        'change': function() {
+                            if (this.get_value()) {
+                                var customer = this.grid_row.on_grid_fields[1];
+                                var customer_name = this.grid_row.on_grid_fields[2];
+                                var currency = this.grid_row.on_grid_fields[3];
+                                var outstanding_amount = this.grid_row.on_grid_fields[4];
+                                frappe.call({
+                                    'method': "frappe.client.get",
+                                    'args': {
+                                        "doctype": "Sales Invoice",
+                                        "name": this.get_value()
+                                    },
+                                    'callback': function(r)
+                                    {
+                                        var sinv = r.message;
+                                        customer.set_value(sinv.customer);
+                                        customer_name.set_value(sinv.customer_name);
+                                        currency.set_value(sinv.currency);
+                                        outstanding_amount.set_value(sinv.outstanding_amount);
+                                        
+                                        recalc_allocation(d);
+                                    }
+                                });
+                            }
+                        },
+                        get_query: function() {
+                            return {
+                                'filters': [ 
+                                    ['company', '=', frm.doc.company],
+                                    ['outstanding_amount', '>', 0],
+                                    ['currency', '=', frm.doc.paid_to_account_currency]
+                                ]
+                            };
+                        }
+                    },
+                    {
+                        'fieldname': 'customer', 
+                        'fieldtype': 'Data', 
+                        'label': __('Customer'),
+                        'in_list_view': 1,
+                        'read_only': 1
+                    },
+                    {
+                        'fieldname': 'customer_name', 
+                        'fieldtype': 'Data', 
+                        'label': __('Customer Name'),
+                        'in_list_view': 1,
+                        'read_only': 1
+                    },
+                    {
+                        'fieldname': 'currency', 
+                        'fieldtype': 'Data', 
+                        'label': __('Currency'),
+                        'in_list_view': 1,
+                        'read_only': 1
+                    },
+                    {
+                        'fieldname': 'outstanding_amount', 
+                        'fieldtype': 'Float', 
+                        'precision': 2,
+                        'label': __('Outstanding Amount'),
+                        'in_list_view': 1,
+                        'read_only': 1
+                    }
+                ],
+                'data': [],
+                'get_data': () => {
+                    return [];
+                }
+            },
+            {
+                'fieldname': 'sec_1', 
+                'fieldtype': 'Section Break', 
+                'label': __('Sum')
+            },
+            {
+                'fieldname': 'allocated', 
+                'fieldtype': 'Float', 
+                'label': __('Allocated'),
+                'default': 0,
+                'read_only': 1
+            },
+        ],
+        primary_action: function(){
+            var amount = recalc_allocation(d);
+            d.hide();
+            var v = d.get_values()
+            // call backend to create the journal entry
+            frappe.call({
+                'method': "microsynth.microsynth.utils.book_avis",
+                'args': {
+                    'company': frm.doc.company,
+                    'intermediate_account': account_matrix[frm.doc.company].unclear[v.currency],
+                    'currency_deviation_account': account_matrix[frm.doc.company].currency,
+                    'invoices': v.invoices,
+                    'amount': amount,
+                    'reference': frm.doc.name
+               },
+               callback: function(response) {
+                    var jv = response.message;
+                    cur_frm.set_value("remarks", cur_frm.doc.remarks + "\nAvis booked in " + jv);
+                    // switch mode to transfer
+                    var paid_to = cur_frm.doc.paid_to;
+                    cur_frm.set_value("payment_type", "Internal Transfer");
+                    setTimeout(function() {
+                        cur_frm.set_value("paid_to", paid_to);
+                        cur_frm.set_value("paid_from", account_matrix[frm.doc.company].unclear[v.currency])
+                    }, 500);
+               }
+            });
+        },
+        primary_action_label: __('OK'),
+        title: __('Allocate Avis')
+    });
+    d.show();
+
+}
+
+function recalc_allocation(d) {
+    var sum = 0;
+    for (var i = 0; i < d.fields[4].data.length; i++) {
+        sum += d.fields[4].data[i].outstanding_amount;
+    }
+    d.set_value("allocated", sum);
+    return sum
+}
