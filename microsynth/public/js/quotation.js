f7f89e14bc62cbe284f843b0c9bcadb9fe58448d
Rolf Suter <rolf.suter@gmx.net>
2023-05-04 16:47:46 +0200

    Limit customer_address on Quotation to billing addresses

--- microsynth/public/js/quotation.js
+++ microsynth/public/js/quotation.js
@@ -5,6 +5,21 @@ frappe.ui.form.on('Quotation Item', {
 });
 
 frappe.ui.form.on('Quotation', {
+    refresh(frm){
+        // run code with a delay because the core framework code is slower than the refresh trigger and would overwrite it
+        setTimeout(function(){
+            cur_frm.fields_dict['customer_address'].get_query = function(doc) {          //gets field you want to filter
+                return {
+                    filters: {
+                        "link_doctype": "Customer",
+                        "link_name": cur_frm.doc.party_name,
+                        "address_type": "Billing"
+                    }
+                }
+            } 
+        },500);  
+    },
+    
     before_save(frm) {
         if (frm.doc.product_type == "Oligos" || frm.doc.product_type == "Material") {
             var category = "Material";
