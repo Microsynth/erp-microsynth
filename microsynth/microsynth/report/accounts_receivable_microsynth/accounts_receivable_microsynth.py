d28555daf703c3783091a58dc436df06203bd399
lasalesi <larsericmueller@gmail.com>
2023-05-12 18:39:41 +0200

    group debtors (microsynth) by ext. debtor number

--- microsynth/microsynth/report/accounts_receivable_microsynth/accounts_receivable_microsynth.py
+++ microsynth/microsynth/report/accounts_receivable_microsynth/accounts_receivable_microsynth.py
@@ -19,7 +19,11 @@ def execute(filters=None):
     new_columns = []
     for c in columns:
         # skip range columns
-        if c['fieldname'].startswith("range"):
+        if c['fieldname'].startswith("range") \
+            or c['fieldname'] == "voucher_type" \
+            or c['fieldname'] == "voucher_no" \
+            or c['fieldname'] == "age" \
+            or c['fieldname'] == "due_date":
             continue
         
         # insert external debtor number
@@ -36,5 +40,21 @@ def execute(filters=None):
     # extend data
     for d in data:
         d['ext_customer'] = frappe.get_cached_value("Customer", d['party'], 'ext_debitor_number')
-        
-    return new_columns, data
+    
+    # now, aggregate by external debitor
+    output = []
+    customer_keys = {}
+    for d in data:
+        customer = d['party']
+        if customer not in customer_keys:
+            # this customer was not seen, add to output
+            customer_keys[customer] = len(output)       # store index to row
+            output.append(d)                                # append this row
+        else:
+            # this customer is already in the list, update
+            output[customer_keys[customer]]['invoiced'] += d['invoiced']
+            output[customer_keys[customer]]['paid'] += d['paid']
+            output[customer_keys[customer]]['credit_note'] += d['credit_note']
+            output[customer_keys[customer]]['outstanding'] += d['outstanding']
+            
+    return new_columns, output
