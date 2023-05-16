4bce0f77b6b8b992496b9a1768f3fff2c1893d0a
Rolf Suter <rolf.suter@gmx.net>
2023-05-16 09:59:51 +0200

    Set naming series for Credit Notes/Returns when creating the document manually

--- microsynth/public/js/sales_invoice.js
+++ microsynth/public/js/sales_invoice.js
@@ -30,6 +30,10 @@ frappe.ui.form.on('Sales Invoice', {
     before_save(frm) {
         // set goodwill period to 10 days
         cur_frm.set_value("exclude_from_payment_reminder_until", frappe.datetime.add_days(frm.doc.due_date, 10));
+    },
+    is_return(frm) {
+        prepare_naming_series(frm);
+        setTimeout(() => { set_naming_series(frm); }, 1000);
     }
 });
 
