96eaf5f8c39ee45bcb693f2acd425ccf4cbe4c6b
Rolf Suter <rolf.suter@gmx.net>
2023-06-07 22:23:29 +0200

    Insert a comment when cloning an invoice

--- microsynth/microsynth/utils.py
+++ microsynth/microsynth/utils.py
@@ -1064,6 +1064,7 @@ def exact_copy_sales_invoice(sales_invoice):
     new.creation = datetime.now()
     new.owner = user.name
     new.insert()
+    comment_invoice(new.name, "Cloned from {0}<br>by {1}".format(original.name, user.name))
     frappe.db.commit()
     return new.name
 
