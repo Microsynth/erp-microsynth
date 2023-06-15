205aebadd437a95b07b2a680958fad4ac3d388e4
Rolf Suter <rolf.suter@gmx.net>
2023-06-15 11:02:11 +0200

    Fall back to invoice_to contact if there is no reminder_to contact

--- microsynth/microsynth/payment_reminder.py
+++ microsynth/microsynth/payment_reminder.py
@@ -10,12 +10,14 @@ After creation of a new payment reminder, extend meta data with specific values
 def extend_values(self, event):
     # fetch payment reminder contact > email id
     if self.customer:
-        payment_reminder_contact = frappe.get_value("Customer", self.customer, "reminder_to")
-        if payment_reminder_contact:
-            email_to = frappe.get_value("Contact", payment_reminder_contact, "email_id")
+        reminder_contact = frappe.get_value("Customer", self.customer, "reminder_to")
+        if not reminder_contact:
+            reminder_contact = frappe.get_value("Customer", self.customer, "invoice_to")
+        if reminder_contact:
+            email_to = frappe.get_value("Contact", reminder_contact, "email_id")
             if email_to:
                 self.email = email_to
                 self.save()
                 frappe.db.commit()
-                
+
     return
