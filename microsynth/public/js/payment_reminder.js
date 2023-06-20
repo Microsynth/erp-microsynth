86b0cf75d54fa8eafcf5f1911dd5c06179e37eb8
Rolf Suter <rolf.suter@gmx.net>
2023-06-20 16:15:35 +0200

    Text for payment reminder emails

--- microsynth/public/js/payment_reminder.js
+++ microsynth/public/js/payment_reminder.js
@@ -13,21 +13,15 @@ frappe.ui.form.on('Payment Reminder', {
 });
 
 function custom_mail_dialog(frm) {
-    var recipient = frm.doc.email || frm.doc.email_id || frm.doc.contact_email || "";
+    var recipient = frm.doc.email;
     var cc = "";
-    var subject = "Microsynth Payment Reminder";
-    /* if (frm.doc.highest_level === 2) {
-        subject = "Mahnung";
-    } else if (frm.doc.highest_level === 3) {
-        subject = "Zweite Mahnung";
-    } */
 
     new frappe.views.CommunicationComposer({
 		doc: {
 		    doctype: frm.doc.doctype,
 		    name: frm.doc.name
 		},
-		subject: subject + " " + frm.doc.name,
+		subject: get_email_subject(frm),
 		cc:  cc,
 		recipients: recipient,
 		attach_document_print: true,
@@ -35,30 +29,89 @@ function custom_mail_dialog(frm) {
 	});
 }
 
-function get_email_body(frm) {
-    var html = "<p>" + __("Dear Sir or Madam", frm.doc.language) + "</p>"
-        + "<p><br></p>";
-    
-    if (frm.doc.highest_level === 1) {
-        html += "<p>Im hektischen Alltag kann es schnell einmal vorkommen eine fällige Zahlung zu übersehen..."
-                + "<br><br>Wir bitten Sie höflich um Zahlung der fälligen angehängten Rechnung in den nächsten 5 Tagen."
-             + "<br><br>Sollten Sie die Zahlung bereits veranlasst haben, bitten wir um Information, wann Ihre Zahlung auf welches Konto erfolgt ist.</p>";
-    } else if (frm.doc.highest_level === 2) {
-        html += "<p>Wir haben Ihnen bereits eine Zahlungserinnerung zukommen lassen."
-             + "<br><br><i>Hat es einen Grund für die ausstehende Zahlung?</i>"
-             + "<br> - Dann kontaktieren Sie uns bitte, wir finden sicher eine gemeinsame Lösung"
-             + "<br><br><i>Haben Sie bereits bezahlt?</i>"
-             + "<br>- Bitte geben Sie uns das Valutadatum und das begünstigte Konto an, damit wir der Zahlung nachgehen können"
-             + "<br><br><i>Haben Sie es einfach vergessen?</i>"
-             +"<br>- Kein Problem, gleichen Sie die Rechnung innert den nächsten 3 Tagen aus. Vielen Dank im Voraus!</p>";
-    } else {
-        html += "<p>Leider konnten wir trotz mehreren Schreiben noch keinen Zahlungseingang für die überfällige Rechnung feststellen. "
-             + "<br><br>Bitte haben Sie dafür Verständnis, dass wir uns als nächsten Schritt weitere Massnahmen vorbehalten."
-             + "<br><br>Bitte vermeiden Sie diese Umtriebe und Kosten und nehmen die Zahlung innert den nächsten drei Tagen vor, vielen Dank im Voraus!</p>";
+function get_email_subject(frm) {
+    if (frm.doc.language == "de") {
+        if (frm.doc.highest_level == 1 ) {
+            var subject = "Microsynth Zahlungserinnerung " + frm.doc.name
+        }
+        else if (frm.doc.highest_level == 2){
+            var subject = "Microsynth Mahnung " + frm.doc.name
+        }
+        else if (frm.doc.highest_level == 3){
+            var subject = "Microsynth Mahnung " + frm.doc.name
+        }
+        else {
+            var subject = "Microsynth Mahnung " + frm.doc.name
+        }
     }
-    html += "<p><br></p>"
-         + "<p>Sollten Sie noch Fragen haben, stehen wir gerne zur Verfügung.</p>"
-         + "<p><br></p>";
-    
+    else {
+        var subject = "Microsynth Payment Reminder " + frm.doc.name
+    }
+    return subject
+}
+
+function get_email_body(frm) {
+    if (frm.doc.language == "de") {
+        var html = "<p>Sehr geehrte Damen und Herren</p>"
+        html += "<p><br></p>";
+
+        if (frm.doc.highest_level == 1 ) {
+            html += "<p>Die nachfolgende(n) Rechnung(en) war(en) bereits zur Zahlung fällig. Vermutlich ist es Ihrer Aufmerksamkeit entgangen, diese rechtzeitig zu begleichen. Wir wären Ihnen sehr dankbar für eine rasche Überweisung.</p>";
+            html += "<p>Sollten Sie Fragen zur Rechnungsaufstellung haben, so freuen wir uns sehr, von Ihnen zu hören. Sollte sich die Zahlung mit dieser Nachricht überschnitten haben, so bitten wir Sie, diese als hinfällig zu betrachten.</p>";
+        }
+        else if (frm.doc.highest_level == 2) {
+            html += "<p>In der Hektik des geschäftlichen Alltags kommt es vor, dass Zahlungen vergessen werden. Gerne erinnern wir Sie daher ein zweites Mal an diese noch offene(n) Rechnung(en). Sollte es Unklarheiten zu der Rechnung geben, so würde es uns freuen, diese mit Ihnen zu klären. Andernfalls wären wir für eine umgehende Überweisung dankbar. Sollte sich die Zahlung mit dieser Nachricht überschnitten haben, so bitten wir Sie, diese als hinfällig zu betrachten.</p>";
+        }
+        else if (frm.doc.highest_level == 3) {
+            html += "<p>In der Hektik des geschäftlichen Alltags kommt es vor, dass Zahlungen vergessen werden. Gerne erinnern wir Sie daher ein zweites Mal an diese noch offene(n) Rechnung(en). Sollte es Unklarheiten zu der Rechnung geben, so würde es uns freuen, diese mit Ihnen zu klären. Andernfalls wären wir für eine umgehende Überweisung dankbar. Sollte sich die Zahlung mit dieser Nachricht überschnitten haben, so bitten wir Sie, diese als hinfällig zu betrachten.</p>";
+        }
+        else {
+            html += "<p>Unserer Buchhaltung zufolge ist diese Rechnung vom 02.12.2019 auch nach unserer 1. Mahnung vom  und 2. Mahnung vom  immer noch nicht beglichen. Bitte überweisen Sie den fälligen Betrag zur Vermeidung weiterer Schritte unverzüglich auf unser Konto. Zahlungseingänge sind bis und mit gestern berücksichtig. Sollte sich Ihre Zahlung mit unserem Schreiben gekreuzt haben, bitten wir Sie, dieses als gegenstandslos zu betrachten.</p>";
+        }
+        html += "<p><br>Beste Grüsse<br>Microsynth Administration<br><br></p>";
+    }
+    // else if (frm.doc.language == "fr") {
+    //     var html = "<p>Dear Sir or Madam</p>"
+    //     html += "<p><br></p>";
+
+    //     if (frm.doc.highest_level == 1 ) {
+    //         html += "<p>The following invoice(s) has(have) already been due for payment. We assume that the punctual payment has escaped your notice and would be grateful for a rapid settlement of the invoice(s).</p>";
+    //         html += "<p>If you have any questions about the invoice(s), please feel free to contact us. If the payment has already been authorized, please ignore this message.</p>";
+    //     }
+    //     else if (frm.doc.highest_level == 2) {
+    //         html += "<p>Everyday business life can be hectic and invoices can be forgotten. We would therefore like to remind you a second time that the following invoice(s) is(are) due for payment. Should you have questions about the invoice(s), please feel free to contact us. Otherwise, we would be grateful for an immediate payment. If the payment has already been authorized, please ignore this message.</p>";
+    //     }
+    //     else if (frm.doc.highest_level == 3) {
+    //         html += "<p>Everyday business life can be hectic and invoices can be forgotten. We would therefore like to remind you a second time that the following invoice(s) is(are) due for payment. Should you have questions about the invoice(s), please feel free to contact us. Otherwise, we would be grateful for an immediate payment. If the payment has already been authorized, please ignore this message.</p>";
+    //     }
+    //     else {
+    //         html += "<p>As per our accounting department, this invoice(s) is (are) due for payment since a long time. Please check the invoice and report any dissension immediately to Microsynth AG.</p>";
+    //     }
+    //     html += "<p><br></p>";
+    //     html += "<p>" + frm.doc.company + "</p>";
+    // }
+    else {
+        var html = "<p>Dear Sir or Madam</p>"
+        html += "<p><br></p>";
+
+        if (frm.doc.highest_level == 1 ) {
+            html += "<p>The following invoice(s) has(have) already been due for payment. We assume that the punctual payment has escaped your notice and would be grateful for a rapid settlement of the invoice(s).</p>";
+            html += "<p>If you have any questions about the invoice(s), please feel free to contact us. If the payment has already been authorized, please ignore this message.</p>";
+        }
+        else if (frm.doc.highest_level == 2) {
+            html += "<p>Everyday business life can be hectic and invoices can be forgotten. We would therefore like to remind you a second time that the following invoice(s) is(are) due for payment. Should you have questions about the invoice(s), please feel free to contact us. Otherwise, we would be grateful for an immediate payment. If the payment has already been authorized, please ignore this message.</p>";
+        }
+        else if (frm.doc.highest_level == 3) {
+            html += "<p>Everyday business life can be hectic and invoices can be forgotten. We would therefore like to remind you a second time that the following invoice(s) is(are) due for payment. Should you have questions about the invoice(s), please feel free to contact us. Otherwise, we would be grateful for an immediate payment. If the payment has already been authorized, please ignore this message.</p>";
+        }
+        else {
+            html += "<p>As per our accounting department, this invoice(s) is (are) due for payment since a long time. Please check the invoice and report any dissension immediately to Microsynth AG.</p>";
+        }
+        html += "<p><br>Best regards<br>Microsynth Administration<br><br></p>";
+    }
+
+    html += "<p><b style=\"color: #e65023;\">Microsynth</b> • Schützenstrasse 15 • CH-9436 Balgach • www.microsynth.com</p>"
+    // html += "<p style=\"font-size: 7pt; color: grey;\">MWST Nr. (VAT): CHE-107.542.107MWST</p>"
+
     return html;
 }
