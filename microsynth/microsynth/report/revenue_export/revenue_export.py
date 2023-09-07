9096467d7a06423a0c47a3ad540c50b3cd7e505f
Rolf Suter <rolf.suter@gmx.net>
2023-09-07 12:36:05 +0200

    Add Item Group filter

--- microsynth/microsynth/report/revenue_export/revenue_export.py
+++ microsynth/microsynth/report/revenue_export/revenue_export.py
@@ -7,7 +7,7 @@ from frappe import _
 from frappe.utils import cint
 import calendar
 from microsynth.microsynth.utils import get_child_territories
-from microsynth.microsynth.report.sales_overview.sales_overview import get_exchange_rate
+from microsynth.microsynth.report.sales_overview.sales_overview import get_exchange_rate, get_item_groups
 
 
 def get_month_number(month):
@@ -32,15 +32,18 @@ def get_columns(filters):
     return [
         {"label": _("Sales Invoice"), "fieldname": "sales_invoice", "fieldtype": "Link", "options": "Sales Invoice", "width": 130 },
         {"label": _("Item"), "fieldname": "item_code", "fieldtype": "Link", "options": "Item", "width": 75 },
+        {"label": _("Item Group"), "fieldname": "item_group", "fieldtype": "Link", "options": "Item Group", "width": 100 },
         {"label": _("Quantity"), "fieldname": "qty", "fieldtype": "Float", "options": "", "precision": "0", "width": 75 },
         {"label": _("Base net amount"), "fieldname": "base_net_amount", "fieldtype": "Currency", "options": "base_currency", "width": 120},
         {"label": _("CHF"), "fieldname": "chf", "fieldtype": "Currency", "options": "currency_chf", "width": 120},
         {"label": _("EUR"), "fieldname": "eur", "fieldtype": "Currency", "options": "currency_eur", "width": 120},
+        {"label": _("Invoice Conversion Rate"), "fieldname": "conversion_rate", "fieldtype": "Float", "precision": "6", "width": 120},
+        {"label": _("Monthly Currency Exchange"), "fieldname": "exchange_rate", "fieldtype": "Float", "precision": "6", "width": 120},
         {"label": _("Company"), "fieldname": "company", "fieldtype": "Link", "options": "Company", "width": 175},
     ]
 
 
-def get_item_revenue(filters, month, debug=False):
+def get_item_revenue(filters, month, item_groups, debug=False):
     company = "%"
     if filters.get("company"):
         company = filters.get("company")
@@ -52,7 +55,7 @@ def get_item_revenue(filters, month, debug=False):
 
     
     last_day = calendar.monthrange(cint(filters.get("fiscal_year")), month)
-
+    group_condition = "'{0}'".format("', '".join(item_groups))
 
     # TODO: replace base_net_amount by a discount-corrected version 'discounted item amount in company currency'
     #     (`tabSales Invoice Item`.`amount` * (`tabSales Invoice`.`total`  - (`tabSales Invoice`.`discount_amount` - `tabSales Invoice`.`total_customer_credit`)) / `tabSales Invoice`.`total`) * `tabSales Invoice`.`conversion_rate` AS `discounted item amount in company currency`,
@@ -64,8 +67,10 @@ def get_item_revenue(filters, month, debug=False):
             SELECT 
                 `tabSales Invoice Item`.`parent` AS `document`,
                 `tabSales Invoice Item`.`base_net_amount` AS `base_net_amount`, 
+                `tabSales Invoice Item`.`item_group` AS `item_group`,
                 `tabSales Invoice Item`.`item_group` AS `remarks`,
-                `tabSales Invoice`.`company`
+                `tabSales Invoice`.`company`,
+                `tabSales Invoice`.`conversion_rate` AS `conversion_rate`
             FROM `tabSales Invoice Item`
             LEFT JOIN `tabSales Invoice` ON `tabSales Invoice Item`.`parent` = `tabSales Invoice`.`name`
             WHERE 
@@ -73,29 +78,36 @@ def get_item_revenue(filters, month, debug=False):
                 AND `tabSales Invoice`.`company` LIKE "{company}"
                 AND `tabSales Invoice`.`posting_date` BETWEEN "{year}-{month:02d}-01" AND "{year}-{month:02d}-{to_day:02d}"
                 AND `tabSales Invoice`.`territory` {territory_condition}
+                AND `tabSales Invoice Item`.`item_group` IN ({group_condition})
             ;
         """.format(company=company, year=filters.get("fiscal_year"), month=month, to_day=last_day[1],
-            territory_condition=territory_condition)
+            territory_condition=territory_condition, group_condition=group_condition)
     items = frappe.db.sql(query, as_dict=True)
     
     return items
 
 
 def get_revenue_details(filters, debug=False):
+    if filters.get("item_group"):
+        item_groups = [ filters.get("item_group") ]
+    else:
+        # TODO get_item_groups does not include shipping
+        item_groups = get_item_groups()
 
     if filters.get("month"): 
         m = get_month_number(filters.get("month"))
-        details = get_item_revenue(filters, m, debug)
+        details = get_item_revenue(filters, m, item_groups, debug)
     else:
         details = []
         for m in range(1, 12 + 1):
-            details += get_item_revenue(filters, month = m, debug=debug)
+            details += get_item_revenue(filters, month = m, item_groups=item_groups, debug=debug)
 
     # add chf and eur columns
     company_currency = {}
     for c in frappe.get_all("Company", fields=['name', 'default_currency']):
         company_currency[c['name']] = c['default_currency']
     
+    # TODO use month to get exchange rate
     exchange_rate = get_exchange_rate(filters.get("fiscal_year"), 7)
 
     for i in details:
