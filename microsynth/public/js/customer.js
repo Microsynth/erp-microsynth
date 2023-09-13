5a7e1d9ad67df9bac7de5daae467cc582e721850
Rolf Suter <rolf.suter@gmx.net>
2023-09-13 16:56:17 +0200

    Move code from Custom Script to code base

--- microsynth/public/js/customer.js
+++ microsynth/public/js/customer.js
@@ -21,6 +21,15 @@ frappe.ui.form.on('Customer', {
             frm.add_custom_button(__("Price List"), function() {
                 frappe.set_route("query-report", "Pricing Configurator", {'price_list': frm.doc.default_price_list});
             });
+        };
+        if ((frm.doc.invoicing_method === "Email") && (!frm.doc.invoice_to)) {
+            fetch_primary_contact(frm);
+        }
+    },
+    validate(frm) {
+        if ((!frm.doc.__islocal) && (frm.doc.invoicing_method === "Email") && (!frm.doc.invoice_to)) {
+            frappe.msgprint( __("Please select an invoice contact"), __("Validation") );
+            frappe.validated=false;
         }
     },
     after_save(frm) {
@@ -35,3 +44,18 @@ frappe.ui.form.on('Customer', {
         });
     }
 });
+
+function fetch_primary_contact(frm) {
+    frappe.call({
+        'method': 'erpnextswiss.scripts.crm_tools.get_primary_customer_contact',
+        'args': {
+            'customer': frm.doc.name
+        },
+        'callback': function(r) {
+            if (r.message) {
+                var contact = r.message;
+                cur_frm.set_value("invoice_to", contact.name);
+            } 
+        }
+    });
+}
