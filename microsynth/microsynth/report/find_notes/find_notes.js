e77cf6fe592c7be915f7390f3865fdc4c3829faa
Jens Petermann <jens.petermann@microsynth.ch>
2023-09-20 10:38:35 +0200

    Created new "Find Notes" Report (still empty), Task #13722 KB ERP

---
+++ microsynth/microsynth/report/find_notes/find_notes.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2023, Microsynth, libracore and contributors and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Find Notes"] = {
+	"filters": [
+
+	]
+};
