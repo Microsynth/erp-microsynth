66d2774461be1f67e490f0d9f26ffd77aaba4cc2
Jens Petermann <jens.petermann@microsynth.ch>
2023-09-28 17:34:53 +0200

    Draft implementation of "Find Tracking Code" Report (Task #13807 KB ERP)

---
+++ microsynth/microsynth/report/find_tracking_code/find_tracking_code.js
@@ -0,0 +1,37 @@
+// Copyright (c) 2023, Microsynth, libracore and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Find Tracking Code"] = {
+	"filters": [
+		{
+            "fieldname": "contact",
+            "label": __("Contact"),
+            "fieldtype": "Link",
+            "options": "Contact"
+        },
+		{
+            "fieldname": "customer",
+            "label": __("Customer"),
+            "fieldtype": "Link",
+            "options": "Customer"
+        },
+		{
+            "fieldname": "sales_order",
+            "label": __("Sales Order"),
+            "fieldtype": "Link",
+            "options": "Sales Order"
+        },
+		{
+            "fieldname": "web_order_id",
+            "label": __("Web Order ID"),
+            "fieldtype": "Data"
+        },
+		{
+            "fieldname": "delivery_note",
+            "label": __("Delivery Note"),
+            "fieldtype": "Link",
+            "options": "Delivery Note"
+        },
+	]
+};
---
+++ microsynth/microsynth/report/find_tracking_code/find_tracking_code.py
@@ -0,0 +1,66 @@
+# Copyright (c) 2023, Microsynth, libracore and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe import _
+
+
+def get_columns(filters):
+    return [
+        {"label": _("Contact"), "fieldname": "contact", "fieldtype": "Link", "options": "Contact", "width": 75 },
+        {"label": _("First Name"), "fieldname": "first_name", "fieldtype": "Data", "width": 100 },
+        {"label": _("Last Name"), "fieldname": "last_name", "fieldtype": "Data", "width": 100 },
+		{"label": _("Customer"), "fieldname": "customer", "fieldtype": "Link", "options": "Customer", "width": 75 },
+        {"label": _("Customer Name"), "fieldname": "customer_name", "fieldtype": "Data", "width": 200 },
+        {"label": _("Sales Order"), "fieldname": "sales_order", "fieldtype": "Link", "options": "Sales Order", "width": 120 },
+        {"label": _("Web Order ID"), "fieldname": "web_order_id", "fieldtype": "Data", "width": 75 },
+        {"label": _("Delivery Note"), "fieldname": "delivery_note", "fieldtype": "Link", "options": "Delivery Note", "width": 125 },
+    ]
+
+
+def get_data(filters):
+    """
+    Get raw Sales Order records for find tracking code report.
+    """
+    contact_condition = customer_condition = sales_order_condition = web_order_id_condition = delivery_note_condition = ""
+
+    if filters and filters.get('contact'):
+        contact_condition = f"AND `tabSales Order`.`contact_person` = '{filters.get('contact')}' "
+    if filters and filters.get('customer'):
+        customer_condition = f"AND `tabSales Order`.`customer` = '{filters.get('customer')}' "
+    if filters and filters.get('sales_order'):
+        sales_order_condition = f"AND `tabSales Order`.`name` = '{filters.get('sales_order')}' "
+    if filters and filters.get('web_order_id'):
+        web_order_id_condition = f"AND `tabSales Order`.`web_order_id` = '{filters.get('web_order_id')}' "
+    if filters and filters.get('delivery_note'):
+        delivery_note_condition = f"AND `tabSales Order`.`name` = '{filters.get('delivery_note')}' "  # TODO
+
+    query = """
+            SELECT
+                `tabSales Order`.`contact_person` AS `contact`,
+                `tabContact`.`first_name`,
+                `tabContact`.`last_name`,
+                `tabSales Order`.`customer` AS `customer`,
+                `tabCustomer`.`customer_name` AS `customer_name`,
+                `tabSales Order`.`name` AS `sales_order`,
+                `tabSales Order`.`web_order_id` AS `web_order_id`
+            FROM `tabSales Order`
+            LEFT JOIN `tabContact` ON `tabContact`.`name` = `tabSales Order`.`contact_person`
+            LEFT JOIN `tabCustomer` ON `tabCustomer`.`name` = `tabSales Order`.`customer`
+            WHERE TRUE
+                {contact_condition}
+                {customer_condition}
+                {sales_order_condition}
+                {web_order_id_condition}
+                {delivery_note_condition}
+        """.format(contact_condition=contact_condition, customer_condition=customer_condition,
+                   sales_order_condition=sales_order_condition, web_order_id_condition=web_order_id_condition,
+                   delivery_note_condition=delivery_note_condition)
+
+    return frappe.db.sql(query, as_dict=True)
+
+
+def execute(filters=None):
+	columns, data = get_columns(filters), get_data(filters)
+	return columns, data
