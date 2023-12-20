5347d38f0540f96f28e4e3279ba3dad47c59e1b0
Jens Petermann <jens.petermann@microsynth.ch>
2023-12-20 15:42:29 +0100

    Created Commission Calculator report (not yet implemented) (Task #14480 KB ERP)

---
+++ microsynth/microsynth/report/commission_calculator/commission_calculator.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2023, Microsynth, libracore and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Commission Calculator"] = {
+	"filters": [
+
+	]
+};
