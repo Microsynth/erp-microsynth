1e6b046624e71bc0075943844941768859711403
Rolf Suter <rolf.suter@gmx.net>
2024-01-25 09:41:43 +0100

    Initial setup of QMS module. Add QM Document.

---
+++ microsynth/config/qms.py
@@ -0,0 +1,30 @@
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+    return[
+        {
+            "label": _("Document Management"),
+            "icon": "octicon octicon-file-submodule",
+            "items": [
+                    {
+                       "type": "doctype",
+                       "name": "QM Document",
+                       "label": _("Document"),
+                       "description": _("Document")
+                    }
+            ]
+        },
+                {
+            "label": _("Complaints"),
+            "icon": "octicon octicon-file-submodule",
+            "items": [
+                    {
+                       "type": "doctype",
+                       "name": "Contact Note",
+                       "label": _("Contact Notes"),
+                       "description": _("Contact Note")
+                    }
+            ]
+        }
+    ]
