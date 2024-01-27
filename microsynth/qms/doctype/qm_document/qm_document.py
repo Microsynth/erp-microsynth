335da3a761ef6b2e5bac19040e06a96993bfa8b8
lasalesi <larsericmueller@gmail.com>
2024-01-27 22:26:20 +0100

    document number generator for QM documents

--- microsynth/qms/doctype/qm_document/qm_document.py
+++ microsynth/qms/doctype/qm_document/qm_document.py
@@ -3,8 +3,33 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import cint
 
 class QMDocument(Document):
-	pass
+    def autoname(self):
+        # auto name function
+        base_name = "{0} {1}.{2}.{3}.".format(
+            self.document_type,
+            self.process_number,
+            self.subprocess_number,
+            self.chapter or "00"
+        )
+        # find document number
+        docs = frappe.db.sql("""
+            SELECT `name` 
+            FROM `tabQM Document`
+            WHERE `name` LIKE "{0}%"
+            LIMIT 1;
+            """.format(base_name), as_dict=True)
+        
+        if len(docs) == 0:
+            doc = 1
+        else:
+            doc = cint((docs[0]['name'][len(base_name):]).split("-")[0]) + 1
+            
+        self.name = "{base}{doc:03d}".format(base=base_name, doc=doc)
+        
+        return
+            
