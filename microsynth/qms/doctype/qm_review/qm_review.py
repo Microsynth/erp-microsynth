a849fa2088fa6a1a1cf06baf6ec32c3bca878538
lasalesi <larsericmueller@gmail.com>
2024-01-30 11:47:51 +0100

    review will update qm document review section

--- microsynth/qms/doctype/qm_review/qm_review.py
+++ microsynth/qms/doctype/qm_review/qm_review.py
@@ -5,10 +5,17 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from datetime import datetime
 
 class QMReview(Document):
-    pass
-
+    def on_submit(self):
+        if self.document_type == "QM Document":
+            ref_doc = frappe.get_doc(self.document_type, self.document_name)
+            ref_doc.reviewed_by = frappe.session.user       # voted over self.modified_by
+            ref_doc.reviewed_on = datetime.now()            # self.modified_on
+            ref_doc.save(ignore_permissions=True)
+            frappe.db.commit()
+        return
 
 @frappe.whitelist()
 def create_review(reviewer, dt, dn, due_date):
