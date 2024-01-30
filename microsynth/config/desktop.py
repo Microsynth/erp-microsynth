91c10a0bc5436bf64435956ada8e53c17c4912d1
lasalesi <larsericmueller@gmail.com>
2024-01-30 08:11:11 +0100

    adding desktop menu

--- microsynth/config/desktop.py
+++ microsynth/config/desktop.py
@@ -3,12 +3,19 @@ from __future__ import unicode_literals
 from frappe import _
 
 def get_data():
-	return [
-		{
-			"module_name": "Microsynth",
-			"color": "grey",
-			"icon": "octicon octicon-file-directory",
-			"type": "module",
-			"label": _("Microsynth")
-		}
-	]
+    return [
+        {
+            "module_name": "Microsynth",
+            "color": "grey",
+            "icon": "octicon octicon-file-directory",
+            "type": "module",
+            "label": _("Microsynth")
+        },
+        {
+            "module_name": "QMS",
+            "color": "grey",
+            "icon": "octicon octicon-star",
+            "type": "module",
+            "label": _("QMS")
+        }
+    ]
