29ca62cfedd2b323449bbf1a03e7247f67ec571e
lasalesi <larsericmueller@gmail.com>
2024-01-30 11:55:38 +0100

    assign reviews to reviewer

--- microsynth/qms/doctype/qm_review/qm_review.py
+++ microsynth/qms/doctype/qm_review/qm_review.py
@@ -6,15 +6,21 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 from datetime import datetime
+from frappe.desk.form.assign_to import add, clear
 
 class QMReview(Document):
     def on_submit(self):
+        # for QM document: update review section
         if self.document_type == "QM Document":
             ref_doc = frappe.get_doc(self.document_type, self.document_name)
             ref_doc.reviewed_by = frappe.session.user       # voted over self.modified_by
             ref_doc.reviewed_on = datetime.now()            # self.modified_on
             ref_doc.save(ignore_permissions=True)
             frappe.db.commit()
+            
+        # clear any assignments
+        clear("Qm Review", self.name)
+        
         return
 
 @frappe.whitelist()
@@ -31,4 +37,11 @@ def create_review(reviewer, dt, dn, due_date):
     review.save(ignore_permissions = True)
     frappe.db.commit()
 
-    return
+    # create assignment to user
+    add({
+        'doctype': "QM Review",
+        'name': review.name,
+        'assign_to': reviewer
+    })
+    
+    return review.name
