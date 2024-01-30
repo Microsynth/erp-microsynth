87a7fb0f54e8e4ab8e678f9b1dd0dc933c35952d
Rolf Suter <rolf.suter@gmx.net>
2024-01-30 09:38:12 +0100

    Add review request functionality

--- microsynth/qms/doctype/qm_document/qm_document.js
+++ microsynth/qms/doctype/qm_document/qm_document.js
@@ -2,7 +2,38 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('QM Document', {
-	// refresh: function(frm) {
-
-	// }
+    refresh: function(frm) {
+        if (true) {
+            frm.add_custom_button(__("Review request"), function() {
+                request_review(frm);
+            });
+        }
+    }
 });
+
+
+function request_review() {
+    frappe.prompt([
+        {'fieldname': 'reviewer', 'fieldtype': 'Link', 'label': __('Reviewer'), 'options':'User', 'reqd': 1},
+        {'fieldname': 'due_date', 'fieldtype': 'Date', 'label': __('Due date'), 'reqd': 1}
+    ],
+    function(values){
+        console.log(values.reviewer);
+        frappe.call({
+            'method': 'microsynth.qms.doctype.qm_review.qm_review.create_review',
+            'args': {
+                'reviewer': values.reviewer,
+                'dt': cur_frm.doc.doctype,
+                'dn': cur_frm.doc.name,
+                'due_date': values.due_date
+            },
+            "callback": function(response) {
+                console.log("created review request...")
+                cur_frm.reload_doc();
+            }
+        })
+    },
+    __('Please choose a reviewer'),
+    __('Request review')
+    )
+}
--- microsynth/qms/doctype/qm_review/qm_review.py
+++ microsynth/qms/doctype/qm_review/qm_review.py
@@ -3,8 +3,25 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class QMReview(Document):
-	pass
+    pass
+
+
+@frappe.whitelist()
+def create_review(reviewer, dt, dn, due_date):
+    review = frappe.get_doc(
+        {
+            'doctype': 'QM Review',
+            'reviewer': reviewer, 
+            'document_type': dt,
+            'document_name': dn,
+            'due_date': due_date
+        })
+
+    review.save(ignore_permissions = True)
+    frappe.db.commit()
+
+    return
