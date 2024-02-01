782e30d14694b59913e00e3be01d13388cc786f4
lasalesi <larsericmueller@gmail.com>
2024-02-01 09:43:18 +0100

    list view status controller

---
+++ microsynth/qms/doctype/qm_document/qm_document_list.js
@@ -0,0 +1,17 @@
+// Copyright (c) 2024, Microsynth, libracore and contributors
+// For license information, please see license.txt
+
+// render
+frappe.listview_settings['QM Document'] = {
+    get_indicator: function(doc) {
+        var status_color = {
+            "Draft": "red",             /* note: all document status draft are overruled by the core to red/draft */
+            "In Review": "orange",
+            "Reviewed": "yellow",
+            "Released": "blue",
+            "Valid": "green",
+            "Invalid": "darkgrey"
+        };
+        return [__(doc.status), status_color[doc.status], "status,=,"+doc.status];
+    }
+};
