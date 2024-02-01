8c25b7757269afa7c824e69a5174d9b4fbf22f42
Rolf Suter <rolf.suter@gmx.net>
2024-02-01 12:28:01 +0100

    Remove chunk

--- microsynth/qms/doctype/qm_training_record/qm_training_record/training_record.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, Microsynth, libracore and contributors and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Training Record', {
-	// refresh: function(frm) {
-
-	// }
-});
