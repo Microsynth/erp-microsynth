5f890dce4f20c962c564166b09bff149193d9f81
lasalesi <larsericmueller@gmail.com>
2024-02-02 11:31:41 +0100

    verify approval password does not match login password

--- microsynth/microsynth/doctype/signature/signature.py
+++ microsynth/microsynth/doctype/signature/signature.py
@@ -1,10 +1,28 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2022, Microsynth, libracore and contributors and contributors
+# Copyright (c) 2022-2024, Microsynth, libracore and contributors and contributors
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils.password import check_password
+from frappe import _
 
 class Signature(Document):
-	pass
+    def validate(self):
+        # if the password has not been updated, skip check
+        if self.approval_password.startswith("***"):
+            return
+        
+        password_match_with_login_pw = True
+        try:
+            check_password(self.user, self.approval_password)
+        except Exception as err:
+            # password failed - this is good, the passwords are different
+            password_match_with_login_pw = False
+            
+        if password_match_with_login_pw:
+            frappe.throw( _("Your approval password must be different from the login password"), _("Validation") )
+
+        return
+
