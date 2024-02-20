bb4c5a0cac671fb5820726b99fc0f154f5623533
lasalesi <larsericmueller@gmail.com>
2024-02-20 10:01:18 +0100

    prevent file uploads after release

--- microsynth/qms/doctype/qm_document/qm_document.js
+++ microsynth/qms/doctype/qm_document/qm_document.js
@@ -52,7 +52,7 @@ frappe.ui.form.on('QM Document', {
         }
 
         // allow to set title, linked documents in specific conditions
-        if (["Draft", "In Review", "Reviewed"].includes(frm.doc.status)) {
+        if (["Draft", "Created", "In Review", "Reviewed"].includes(frm.doc.status)) {
             cur_frm.set_df_property('title', 'read_only', false);
             cur_frm.set_df_property('linked_documents', 'read_only', false);
         }
@@ -175,6 +175,14 @@ frappe.ui.form.on('QM Document', {
         var new_btns = document.getElementsByClassName("btn-new");
         for (var i = 0; i < new_btns.length; i++) {
             new_btns[i].style.visibility = "hidden";
+        }
+        
+        // remove attach file depending on status
+        if (!["Draft", "Created", "In Review", "Reviewed"].includes(frm.doc.status)) {
+            var attach_btns = document.getElementsByClassName("add-attachment");
+            for (var i = 0; i < attach_btns.length; i++) {
+                attach_btns[i].style.visibility = "hidden";
+            }
         }
     }
 });
