419fd6c2c49225b14b317882ebcd68315a572afa
Jens Petermann <jens.petermann@microsynth.ch>
2024-02-26 14:57:20 +0100

    Print City or Postal Code if one of both is missing (Task #15360 KB ERP)

--- microsynth/microsynth/shipping.py
+++ microsynth/microsynth/shipping.py
@@ -108,10 +108,13 @@ def create_receiver_address_lines(customer_name, contact, address):
         if address_doc.address_line1: rec_adr_lines.append(address_doc.address_line1)
         if address_doc.address_line2: rec_adr_lines.append(address_doc.address_line2)
     
-        if address_doc.country in ['United Kingdom']: 
-            if address_doc.city and address_doc.pincode: rec_adr_lines.append(address_doc.city + " " + address_doc.pincode)
-        else: 
-            if address_doc.pincode and address_doc.city: rec_adr_lines.append(address_doc.pincode + " " + address_doc.city)
+        if address_doc.city and address_doc.pincode:
+            if address_doc.country in ['United Kingdom']:
+                rec_adr_lines.append(address_doc.city + " " + address_doc.pincode)
+            else:
+                rec_adr_lines.append(address_doc.pincode + " " + address_doc.city)
+        elif address_doc.city: rec_adr_lines.append(address_doc.city)
+        elif address_doc.pincode: rec_adr_lines.append(address_doc.pincode)
     
         if address_doc.country: rec_adr_lines.append(address_doc.country)
 
