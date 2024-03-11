bbcb7941f3847b9531b9ab28897c6a8f7a99a204
Jens Petermann <jens.petermann@microsynth.ch>
2024-03-11 16:43:21 +0100

    New, still empty report Benchmarking Information (Task #15480 KB ERP)

---
+++ microsynth/microsynth/report/benchmarking_information/benchmarking_information.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2024, Microsynth, libracore and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Benchmarking Information"] = {
+	"filters": [
+
+	]
+};
