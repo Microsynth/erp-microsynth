45fab0cba9ddcccf26c80a9ac99ecf669f745bf6
Jens Petermann <jens.petermann@microsynth.ch>
2024-05-06 15:34:36 +0200

    Prepared printing of shipping label immediatly after picking last label for given item codes (still commented out)

--- microsynth/microsynth/report/open_label_orders/open_label_orders.js
+++ microsynth/microsynth/report/open_label_orders/open_label_orders.js
@@ -215,6 +215,17 @@ function second_barcode_dialog() {
                     frappe.msgprint("invalid barcode range");
                 }
                 locals.label_queue[0].to_barcode = to_barcode;
+                
+                // if (['3100', '3110', '3120', '3200', '3236', '3240', '3251'].includes(locals.label_queue[0].item_code)) {
+                //     // print shipping label
+                //     frappe.call({
+                //         "method": "microsynth.microsynth.labels.print_shipping_label",
+                //         "args": {
+                //             "sales_order_id": locals.label_queue[0].sales_order
+                //         }
+                //     });
+                // }
+                
                 process_queue();
             },
             __("Pick last label"),
