3e25ed515afea2745308f353823710d351ff8974
Jens Petermann <jens.petermann@microsynth.ch>
2024-05-24 10:59:04 +0200

    New, still empty report "Shipped Labels Summary" (Task #16178 KB ERP)

---
+++ microsynth/microsynth/report/shipped_labels_summary/shipped_labels_summary.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2024, Microsynth, libracore and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Shipped Labels Summary"] = {
+	"filters": [
+
+	]
+};
