88c8a3fc70bcd33291f403b8441ad59df346cc53
Jens Petermann <jens.petermann@microsynth.ch>
2024-05-27 10:56:48 +0200

    Exclude Sales Orders without a comment from Customers with Invoicing Method "Stripe Prepayment" from Orders on Hold report (Task #16223 KB ERP)

--- microsynth/microsynth/report/orders_on_hold/orders_on_hold.py
+++ microsynth/microsynth/report/orders_on_hold/orders_on_hold.py
@@ -12,12 +12,13 @@ def execute(filters=None):
 
 def get_columns():
     return [
-        {"label": _("Sales Order"), "fieldname": "sales_order", "fieldtype": "Link", "options": "Sales Order", "width": 120},
-        {"label": _("Web ID"), "fieldname": "web_order_id", "fieldtype": "Data", "width": 70},
+        {"label": _("Sales Order"), "fieldname": "sales_order", "fieldtype": "Link", "options": "Sales Order", "width": 125},
+        {"label": _("Web Order ID"), "fieldname": "web_order_id", "fieldtype": "Data", "width": 90},
         {"label": _("Punchout"), "fieldname": "is_punchout", "fieldtype": "Check", "width": 55},
-        {"label": _("Customer"), "fieldname": "customer", "fieldtype": "Link", "options": "Customer", "width": 70},
+        {"label": _("Customer"), "fieldname": "customer", "fieldtype": "Link", "options": "Customer", "width": 75},
         {"label": _("Customer name"), "fieldname": "customer_name", "fieldtype": "Data", "width": 180},
-        {"label": _("Contact"), "fieldname": "contact", "fieldtype": "Link", "options": "Contact", "width": 60},
+        {"label": _("Invoicing Method"), "fieldname": "invoicing_method", "fieldtype": "Data", "width": 120},
+        {"label": _("Contact"), "fieldname": "contact", "fieldtype": "Link", "options": "Contact", "width": 70},
         {"label": _("Contact name"), "fieldname": "contact_name", "fieldtype": "Data", "width": 150},
         {"label": _("Date"), "fieldname": "date", "fieldtype": "Date", "width": 80},
         {"label": _("Comment"), "fieldname": "comment", "fieldtype": "Data", "width": 600}
@@ -32,6 +33,7 @@ def get_data(filters=None):
             `tabSales Order`.`is_punchout` AS `is_punchout`,
             `tabSales Order`.`customer` AS `customer`,
             `tabSales Order`.`customer_name` AS `customer_name`,
+            `tabCustomer`.`invoicing_method` AS `invoicing_method`,
             `tabSales Order`.`contact_person` AS `contact`,
             `tabSales Order`.`contact_display` AS `contact_name`,
             `tabSales Order`.`transaction_date` AS `date`,
@@ -40,6 +42,7 @@ def get_data(filters=None):
         FROM `tabSales Order`
         LEFT JOIN `tabAddress` ON `tabAddress`.`name` = `tabSales Order`.`shipping_address_name`
         LEFT JOIN `tabCountry` ON `tabCountry`.`name` = `tabAddress`.`country`
+        LEFT JOIN `tabCustomer` ON `tabCustomer`.`name` = `tabSales Order`.`customer`
         WHERE
             `tabSales Order`.`docstatus` = 1
             AND `tabSales Order`.`hold_order` = 1
@@ -47,5 +50,11 @@ def get_data(filters=None):
         ORDER BY
             `tabSales Order`.`transaction_date` DESC;
     """, as_dict=True)
+    filtered_orders = []
+    for order in open_oligo_orders:
+        if order['invoicing_method'] == 'Stripe Prepayment' and not order['comment']:
+            continue
+        else:
+            filtered_orders.append(order)
     
-    return open_oligo_orders
+    return filtered_orders
