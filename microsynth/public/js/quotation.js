b13c1183740ae0aa4d9e7349e6e0eeb59fa44748
Jens Petermann <jens.petermann@microsynth.ch>
2024-06-12 08:30:06 +0200

    Fix Beautification of Internal Note on Quotation banner (Task #14898 KB ERP)

--- microsynth/public/js/quotation.js
+++ microsynth/public/js/quotation.js
@@ -21,8 +21,8 @@ frappe.ui.form.on('Quotation', {
         // Display internal Item notes in a green banner if the Quotation is in Draft status
         if (frm.doc.docstatus == 0 && frm.doc.items.length > 0) {
             var dashboard_comment_color = 'green';
+            cur_frm.dashboard.add_comment("<br>", dashboard_comment_color, true)
             for (var i = 0; i < frm.doc.items.length; i++) {
-                cur_frm.dashboard.add_comment("<br>", dashboard_comment_color, false)
                 frappe.call({
                     'method': "frappe.client.get",
                     'args': {
