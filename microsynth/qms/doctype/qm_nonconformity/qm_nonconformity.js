1180ba6cdf2ed0e345f27bbe0284b7bfc6cc2bea
Jens Petermann <jens.petermann@microsynth.ch>
2024-06-21 14:39:09 +0200

    Add status field and automatic risk classification

--- microsynth/qms/doctype/qm_nonconformity/qm_nonconformity.js
+++ microsynth/qms/doctype/qm_nonconformity/qm_nonconformity.js
@@ -1,8 +1,52 @@
-// Copyright (c) 2024, Microsynth, libracore and contributors and contributors
+// Copyright (c) 2024, Microsynth, libracore and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('QM Nonconformity', {
-	// refresh: function(frm) {
 
-	// }
+frappe.ui.form.on('QM Nonconformity', {
+    // refresh: function(frm) {
+
+    // },
+    occurrence_probability: function(frm) {
+        if (frm.doc.occurrence_probability && frm.doc.impact) {
+            cur_frm.set_value("risk_classification", calculate_risk_classification(frm.doc.occurrence_probability, frm.doc.impact));
+        }
+    },
+    impact: function(frm) {
+        if (frm.doc.occurrence_probability && frm.doc.impact) {
+            cur_frm.set_value("risk_classification", calculate_risk_classification(frm.doc.occurrence_probability, frm.doc.impact));
+        }
+    },
+    occurrence_probability_after_actions: function(frm) {
+        if (frm.doc.occurrence_probability_after_actions && frm.doc.impact_after_actions) {
+            cur_frm.set_value("risk_classification_after_actions", calculate_risk_classification(frm.doc.occurrence_probability_after_actions, frm.doc.impact_after_actions));
+        }
+    },
+    impact_after_actions: function(frm) {
+        if (frm.doc.occurrence_probability_after_actions && frm.doc.impact_after_actions) {
+            cur_frm.set_value("risk_classification_after_actions", calculate_risk_classification(frm.doc.occurrence_probability_after_actions, frm.doc.impact_after_actions));
+        }
+    }
 });
+
+
+function calculate_risk_classification(occ_prob, impact) {
+    const values = new Map([
+        ["small", 1],
+        ["medium", 2],
+        ["high", 3],
+        ["neglectable", 1],
+        ["noticable", 2],
+        ["severe", 3]
+    ])
+    var res = values.get(occ_prob) * values.get(impact);
+    if (res < 3) {
+        return "no actions";
+    } else if (res < 6) {
+        return "check for risk mitigation";
+    } else if (res >= 6) {
+        return "actions required";
+    } else {
+        console.log(res);
+        return "";
+    }
+}
