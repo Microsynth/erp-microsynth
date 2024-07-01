edef8e3bb0222e03c1a9aeec0de1c23945292652
Jens Petermann <jens.petermann@microsynth.ch>
2024-07-01 14:34:31 +0200

    Add new Child Table DocType "QM Change Customer" and more fields to QM Change

--- microsynth/qms/doctype/qm_change/qm_change.js
+++ microsynth/qms/doctype/qm_change/qm_change.js
@@ -2,7 +2,16 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('QM Change', {
-	// refresh: function(frm) {
+	refresh: function(frm) {
 
-	// }
+		// avoid manual changes to some fields
+        cur_frm.set_df_property('created_by', 'read_only', true);
+        cur_frm.set_df_property('created_on', 'read_only', true);
+
+        if (frm.doc.__islocal) {
+            cur_frm.set_value("created_by", frappe.session.user);
+            cur_frm.set_value("created_on", frappe.datetime.get_today());
+        }
+
+	}
 });
