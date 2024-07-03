eff7c2d7cc46e4625eca4b52c3682837c0e219ff
Jens Petermann <jens.petermann@microsynth.ch>
2024-07-03 15:05:26 +0200

    QM Change: Make some fields properly read only; change Text Editor fields to Text fields to avoid "empty" HTML

--- microsynth/qms/doctype/qm_change/qm_change.js
+++ microsynth/qms/doctype/qm_change/qm_change.js
@@ -4,12 +4,6 @@
 frappe.ui.form.on('QM Change', {
 	refresh: function(frm) {
 
-		// avoid manual changes to some fields
-        cur_frm.set_df_property('document_type', 'read_only', true);
-        cur_frm.set_df_property('document_name', 'read_only', true);
-        cur_frm.set_df_property('created_by', 'read_only', true);
-        cur_frm.set_df_property('created_on', 'read_only', true);
-
         if (frm.doc.__islocal) {
             cur_frm.set_value("created_by", frappe.session.user);
             cur_frm.set_value("created_on", frappe.datetime.get_today());
