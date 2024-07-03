71b8571788a9cd7e36e9fedcf1926e214b4ea94f
Jens Petermann <jens.petermann@microsynth.ch>
2024-07-03 12:44:11 +0200

    QM Action: Implemented status changes

--- microsynth/qms/doctype/qm_action/qm_action.js
+++ microsynth/qms/doctype/qm_action/qm_action.js
@@ -2,9 +2,14 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('QM Action', {
-	refresh: function(frm) {
+    refresh: function(frm) {
 
-		// Only creator and QAU can change these fields in Draft status: Title, NC Type, Process, Date, Company, Web Order ID
+        if (!frm.doc.__islocal) {
+            cur_frm.page.clear_primary_action();
+            cur_frm.page.clear_secondary_action();
+        }
+
+        // Only creator and QAU can change these fields in Draft status: Title, NC Type, Process, Date, Company, Web Order ID
         if (!(["Draft"].includes(frm.doc.status) && (frappe.session.user === frm.doc.created_by || frappe.user.has_role('QAU')))) {
             cur_frm.set_df_property('title', 'read_only', true);
             cur_frm.set_df_property('type', 'read_only', true);
@@ -35,7 +40,48 @@ frappe.ui.form.on('QM Action', {
             );
         }
 
-	}
+        if (frm.doc.status == 'Draft' && (frappe.session.user === frm.doc.responsible_person || frappe.user.has_role('QAU'))) {
+            if (frm.doc.title
+                && frm.doc.type
+                && frm.doc.description
+                && frm.doc.qm_process
+                && frm.doc.initiation_date
+                && frm.doc.due_date
+                && frm.doc.responsible_person) {
+                // add submit button
+                cur_frm.page.set_primary_action(
+                    __("Submit"),
+                    function() {
+                        submit();
+                    }
+                );
+            } else {
+                frm.dashboard.add_comment( __("Please set and save Title, Type, Process, Initiation date, Due Date and Description to submit this QM Action."), 'red', true);
+            }
+        }        
+
+        if (frm.doc.status == 'Created' && (frappe.session.user === frm.doc.responsible_person || frappe.user.has_role('QAU'))) {
+            // add start working button
+            cur_frm.page.set_primary_action(
+                __("Start working"),
+                function() {
+                    set_status('Work in Progress');
+                }
+            );
+        }
+
+        if (frm.doc.status == 'Work in Progress' && (frappe.session.user === frm.doc.responsible_person || frappe.user.has_role('QAU'))) {
+            // add complete button
+            cur_frm.page.set_primary_action(
+                __("Complete"),
+                function() {
+                    set_status('Completed');
+                    cur_frm.set_value("closure_date", frappe.datetime.get_today());
+                    cur_frm.reload_doc();
+                }
+            );
+        }
+    }
 });
 
 
@@ -52,19 +98,48 @@ function change_responsible_person() {
         function(values){
             cur_frm.set_value("responsible_person", values.new_responsible_person);
             cur_frm.save();
-			frappe.call({
-				'method': 'microsynth.qms.doctype.qm_action.qm_action.assign',
-				'args': {
-					'doc': cur_frm.doc.name,
-					'responsible_person': values.new_responsible_person
-				},
-				'async': false,
-				'callback': function(response) {
-					cur_frm.reload_doc();
-				}
-			});
+            frappe.call({
+                'method': 'microsynth.qms.doctype.qm_action.qm_action.assign',
+                'args': {
+                    'doc': cur_frm.doc.name,
+                    'responsible_person': values.new_responsible_person
+                },
+                'async': false,
+                'callback': function(response) {
+                    cur_frm.reload_doc();
+                }
+            });
         },
         __('Set new responsible person'),
         __('Set')
     );
+}
+
+function submit(frm) {
+    frappe.call({
+        'method': 'microsynth.qms.doctype.qm_action.qm_action.set_created',
+        'args': {
+            'doc': cur_frm.doc.name,
+            'user': frappe.session.user
+        },
+        'async': false,
+        'callback': function(response) {
+            cur_frm.reload_doc();
+        }
+    });
+}
+
+function set_status(status) {
+    frappe.call({
+        'method': 'microsynth.qms.doctype.qm_action.qm_action.set_status',
+        'args': {
+            'doc': cur_frm.doc.name,
+            'user': frappe.session.user,
+            'status': status
+        },
+        'async': false,
+        'callback': function(response) {
+            cur_frm.reload_doc();
+        }
+    });
 }
--- microsynth/qms/doctype/qm_action/qm_action.py
+++ microsynth/qms/doctype/qm_action/qm_action.py
@@ -7,6 +7,8 @@ import frappe
 from frappe.model.document import Document
 from frappe.desk.form.assign_to import add, clear
 from frappe.utils.data import today
+from microsynth.microsynth.utils import user_has_role
+
 
 class QMAction(Document):
 	pass
@@ -36,6 +38,40 @@ def create_action(title, responsible_person, dt, dn, qm_process, due_date, type,
     assign(action.name, responsible_person)
 
     return action.name
+
+
+@frappe.whitelist()
+def set_created(doc, user):
+    # pull selected document
+    action = frappe.get_doc(frappe.get_doc("QM Action", doc))
+    update_status(action.name, "Created")
+    action.submit()
+    frappe.db.commit()
+
+
+@frappe.whitelist()
+def set_status(doc, user, status):
+    responsible_person = frappe.get_doc(frappe.get_doc("QM Action", doc, "responsible_person"))
+    if not (user == responsible_person or user_has_role(user, "QAU")):
+        frappe.throw(f"Only the Responsible Person or QAU is allowed to set a QM Action to Status '{status}'.")
+    update_status(doc, status)
+
+
+def update_status(action, status):
+    action = frappe.get_doc("QM Action", action)
+    if action.status == status:
+        return
+
+    # validate status transitions
+    if ((action.status == 'Draft' and status == 'Created') or
+        (action.status == 'Created' and status == 'Work in Progress') or
+        (action.status == 'Work in Progress' and status == 'Completed')
+       ):
+        action.status = status
+        action.save()
+        frappe.db.commit()
+    else: 
+        frappe.throw(f"Update QM Action: Status transition is not allowed {action.status} --> {status}")
 
 
 @frappe.whitelist()
