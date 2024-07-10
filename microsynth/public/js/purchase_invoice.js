290c77c270395a4b577f911a94f8927af5ae4148
Jens Petermann <jens.petermann@microsynth.ch>
2024-07-10 17:57:00 +0200

    Implemented a button and functions to request an approval for a Purchase Invoice (Task #16622 KB ERP)

--- microsynth/public/js/purchase_invoice.js
+++ microsynth/public/js/purchase_invoice.js
@@ -4,6 +4,18 @@ frappe.ui.form.on('Purchase Invoice', {
         if (frm.doc.__islocal) {
             prepare_naming_series(frm);             // common function
         }
+
+        if (!frm.doc.__islocal && frm.doc.docstatus == 0 && !frm.doc.in_approval) {
+            frm.add_custom_button(__("Request Approval"), function() {
+                request_approval(frm);
+            });
+        }
+
+        if (frm.doc.in_approval) {
+            cur_frm.set_df_property('approver', 'read_only', true);
+        } else {
+            cur_frm.set_df_property('approver', 'read_only', false);
+        }
         
         hide_in_words();
     },
@@ -13,3 +25,32 @@ frappe.ui.form.on('Purchase Invoice', {
         }            
     }
 });
+
+
+function request_approval(frm) {
+    frappe.prompt([
+        {'fieldname': 'approver', 'fieldtype': 'Link', 'label': __('Approver'), 'options':'User', 'default': cur_frm.doc.approver, 'reqd': 1}
+    ],
+    function(values){
+        frappe.call({
+            'method': 'microsynth.microsynth.invoicing.create_approval_request',
+            'args': {
+                'approver': values.approver,
+                'dt': cur_frm.doc.doctype,
+                'dn': cur_frm.doc.name
+            },
+            "callback": function(response) {
+                if (response.message) {
+                    cur_frm.set_value("in_approval", 1);
+                    frappe.show_alert( __("Approval request created") );
+                    cur_frm.reload_doc();
+                } else {
+                    frappe.show_alert( __("This Purchase Invoice seems to be already assigned to an approver.") );
+                }
+            }
+        });
+    },
+    __('Please choose an approver'),
+    __('Request approval')
+    )
+}
