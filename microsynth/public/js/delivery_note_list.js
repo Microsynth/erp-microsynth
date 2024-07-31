fb71afb5f2b256cab4c3a097cd3d044fca89b086
Jens Petermann <jens.petermann@microsynth.ch>
2024-07-31 08:25:14 +0200

    Reduce code duplication for button to clear filters

--- microsynth/public/js/delivery_note_list.js
+++ microsynth/public/js/delivery_note_list.js
@@ -4,13 +4,6 @@
 // render
 frappe.listview_settings['Delivery Note'] = {
     onload: function(doc) {
-        var filter_bar = document.getElementsByClassName("page-form");
-        var btn_clear = document.createElement("div");
-        btn_clear.setAttribute('class', 'form-group frappe-control input-max-width col-md-2');
-        btn_clear.innerHTML = "<button id='btn_clear' class='btn text-muted'>Clear</button>";
-        for (var i = 0; i < filter_bar.length; i++) {
-            filter_bar[i].appendChild(btn_clear);
-            document.getElementById("btn_clear").onclick = clear_filters;
-        }
+        add_clear_button();
     }
 };
