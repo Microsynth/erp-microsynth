8cee31b5fa8092563b59b72716ccdd5e8aeed9ed
Jens Petermann <jens.petermann@microsynth.ch>
2024-07-31 10:51:13 +0200

    Restore core list controller for Sales Invoice List

--- microsynth/public/js/sales_invoice_list.js
+++ microsynth/public/js/sales_invoice_list.js
@@ -5,5 +5,22 @@
 frappe.listview_settings['Sales Invoice'] = {
     onload: function(doc) {
         add_clear_button();
-    }
+    },
+    add_fields: ["customer", "customer_name", "base_grand_total", "outstanding_amount", "due_date", "company",
+        "currency", "is_return"],
+    get_indicator: function(doc) {
+        var status_color = {
+            "Draft": "grey",
+            "Unpaid": "orange",
+            "Paid": "green",
+            "Return": "darkgrey",
+            "Credit Note Issued": "darkgrey",
+            "Unpaid and Discounted": "orange",
+            "Overdue and Discounted": "red",
+            "Overdue": "red"
+
+        };
+        return [__(doc.status), status_color[doc.status], "status,=,"+doc.status];
+    },
+    right_column: "grand_total"
 };
