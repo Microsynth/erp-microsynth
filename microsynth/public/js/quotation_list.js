868c3bf5b3dae76d4ccd32f5ed63b6dc68303049
Jens Petermann <jens.petermann@microsynth.ch>
2024-08-05 17:07:09 +0200

    Reduce code duplication when adding Clear button on QTN, SO, DN, SI list

--- microsynth/public/js/quotation_list.js
+++ microsynth/public/js/quotation_list.js
@@ -1,35 +1,11 @@
 // Copyright (c) 2024, Microsynth, libracore and contributors
 // For license information, please see license.txt
 
-// render
-frappe.listview_settings['Quotation'] = {
-    add_fields: ["customer_name", "base_grand_total", "status",
-        "company", "currency", 'valid_till'],
 
-    onload: function(listview) {
-        if (listview.page.fields_dict.quotation_to) {
-            listview.page.fields_dict.quotation_to.get_query = function() {
-                return {
-                    "filters": {
-                        "name": ["in", ["Customer", "Lead"]],
-                    }
-                };
-            };
-        }
-        add_clear_button();
-    },
-
-    get_indicator: function(doc) {
-        if(doc.status==="Open") {
-            if (doc.valid_till && doc.valid_till < frappe.datetime.nowdate()) {
-                return [__("Expired"), "darkgrey", "valid_till,<," + frappe.datetime.nowdate()];
-            } else {
-                return [__("Open"), "orange", "status,=,Open"];
-            }
-        } else if(doc.status==="Ordered") {
-            return [__("Ordered"), "green", "status,=,Ordered"];
-        } else if(doc.status==="Lost") {
-            return [__("Lost"), "darkgrey", "status,=,Lost"];
-        }
+var global_onload = frappe.listview_settings['Quotation'].onload;
+frappe.listview_settings['Quotation'].onload = function (doclist) {
+    if (global_onload) {
+        global_onload(doclist);
     }
-};
+    add_clear_button();
+}
--- microsynth/public/js/sales_invoice_list.js
+++ microsynth/public/js/sales_invoice_list.js
@@ -1,26 +1,12 @@
 // Copyright (c) 2024, Microsynth, libracore and contributors
 // For license information, please see license.txt
 
-// render
-frappe.listview_settings['Sales Invoice'] = {
-    onload: function(doc) {
-        add_clear_button();
-    },
-    add_fields: ["customer", "customer_name", "base_grand_total", "outstanding_amount", "due_date", "company",
-        "currency", "is_return"],
-    get_indicator: function(doc) {
-        var status_color = {
-            "Draft": "grey",
-            "Unpaid": "orange",
-            "Paid": "green",
-            "Return": "darkgrey",
-            "Credit Note Issued": "darkgrey",
-            "Unpaid and Discounted": "orange",
-            "Overdue and Discounted": "red",
-            "Overdue": "red"
 
-        };
-        return [__(doc.status), status_color[doc.status], "status,=,"+doc.status];
-    },
-    right_column: "grand_total"
-};
+var global_onload = frappe.listview_settings['Sales Invoice'].onload;
+frappe.listview_settings['Sales Invoice'].onload = function (doclist) {
+    // Precaution in case onload event is added to sales_invoice.js in the future
+    if (global_onload) {
+        global_onload(doclist);
+    }
+    add_clear_button();
+}
