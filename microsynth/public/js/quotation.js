3c0fee618f09627b70a11ed4717fa7b97ba9d40a
Jens Petermann <jens.petermann@microsynth.ch>
2024-08-05 17:47:35 +0200

    Do not show internal item notes on Quotation Drafts to Users with the role "NGS Lab User" due to missing write access

--- microsynth/public/js/quotation.js
+++ microsynth/public/js/quotation.js
@@ -25,7 +25,7 @@ frappe.ui.form.on('Quotation', {
         }
 
         // Display internal Item notes in a green banner if the Quotation is in Draft status
-        if (frm.doc.docstatus == 0 && frm.doc.items.length > 0) {
+        if (frm.doc.docstatus == 0 && frm.doc.items.length > 0 && !frappe.user.has_role("NGS Lab User")) {
             var dashboard_comment_color = 'green';
             //cur_frm.dashboard.add_comment("<br>", dashboard_comment_color, true)
             for (var i = 0; i < frm.doc.items.length; i++) {
