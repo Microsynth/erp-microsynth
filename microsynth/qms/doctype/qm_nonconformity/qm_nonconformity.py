f495115b36599e35fa1478fc0c76360b18c1313f
Rolf Suter <rolf.suter@gmx.net>
2024-08-30 07:33:06 +0200

    Do not throw an error if a Nonconformity is missing.

--- microsynth/qms/doctype/qm_nonconformity/qm_nonconformity.py
+++ microsynth/qms/doctype/qm_nonconformity/qm_nonconformity.py
@@ -374,9 +374,9 @@ def fetch_nonconformities(nonconformity_ids):
     """
     bench execute microsynth.qms.doctype.qm_nonconformity.qm_nonconformity.fetch_nonconformities --kwargs "{'nonconformity_ids': ['NC-240011', 'NC-240002']}"
     """
-    for nc in nonconformity_ids:
-        if not frappe.db.exists("QM Nonconformity", nc):
-            return {'success': False, 'message': f"At least QM Nonconformity '{nc}' does not exist.", 'nonconformities': None}
+    # for nc in nonconformity_ids:
+    #     if not frappe.db.exists("QM Nonconformity", nc):
+    #         return {'success': False, 'message': f"At least QM Nonconformity '{nc}' does not exist.", 'nonconformities': None}
 
     nonconformities = frappe.get_all("QM Nonconformity", filters=[['name', 'IN', nonconformity_ids]], fields=['name', 'title', 'nc_type', 'date', 'description'])
 
