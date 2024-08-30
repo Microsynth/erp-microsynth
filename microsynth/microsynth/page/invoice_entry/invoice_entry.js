3f1da7d2f22e46c2e08db7e3920ff5cdc9d74660
lasalesi <larsericmueller@gmail.com>
2024-08-30 12:45:29 +0200

    prepareation for invoice entry fields (todo: correct date mapping, save function

--- microsynth/microsynth/page/invoice_entry/invoice_entry.js
+++ microsynth/microsynth/page/invoice_entry/invoice_entry.js
@@ -1,17 +1,16 @@
 frappe.pages['invoice_entry'].on_page_load = function(wrapper) {
-	var page = frappe.ui.make_app_page({
-		parent: wrapper,
-		title: 'Invoice Entry',
-		single_column: true
-	});
+    var page = frappe.ui.make_app_page({
+        parent: wrapper,
+        title: 'Invoice Entry',
+        single_column: true
+    });
 
-	frappe.invoice_entry.make(page);
-    frappe.invoice_entry.run();
+    frappe.invoice_entry.make(page);
+    frappe.invoice_entry.run(page);
 
     // add the application reference
     frappe.breadcrumbs.add("Microsynth");
 }
-
 
 frappe.invoice_entry = {
     start: 0,
@@ -22,7 +21,7 @@ frappe.invoice_entry = {
         var data = "";
         $(frappe.render_template('invoice_entry', data)).appendTo(me.body);
     },
-    run: function() { 
+    run: function(page) { 
         frappe.invoice_entry.get_purchase_invoice_drafts();
     },
     get_purchase_invoice_drafts: function() {
@@ -36,12 +35,48 @@ frappe.invoice_entry = {
     },
     display_purchase_invoice_drafts: function(purchase_invoice_drafts) {
         // create rows
-        var html = "";
+        let html = "";
         document.getElementById("pi_drafts_view").innerHTML = "";
-        for (var i = 0; i < purchase_invoice_drafts.length; i++) {
+        for (let i = 0; i < purchase_invoice_drafts.length; i++) {
             html += purchase_invoice_drafts[i].html;
         }
         // insert content
         document.getElementById("pi_drafts_view").innerHTML = html;
+        
+        for (let i = 0; i < purchase_invoice_drafts.length; i++) {
+            frappe.invoice_entry.create_fields(purchase_invoice_drafts[i]);
+        }
+    },
+    create_fields: function(purchase_invoice) {
+        // supplier link field
+        let supplier_fieldname = "supplier_" + purchase_invoice.name;
+        let supplier_field = document.getElementById(supplier_fieldname);
+        let supplier_link_field = frappe.ui.form.make_control({
+            'parent': supplier_field,
+            'df': {
+                'fieldtype': "Link",
+                'fieldname': supplier_fieldname,
+                'options': 'Supplier',
+                'placeholder': __("Supplier")
+            }
+        });
+        supplier_link_field.refresh();
+        supplier_link_field.set_value(purchase_invoice.supplier);
+        
+        // posting date
+        let posting_date_fieldname = "posting_date_" + purchase_invoice.name;
+        let posting_date_field = document.getElementById(posting_date_fieldname);
+        let posting_date_link_field = frappe.ui.form.make_control({
+            'parent': posting_date_field,
+            'df': {
+                'fieldtype': "Date",
+                'fieldname': posting_date_fieldname,
+                'placeholder': __("Posting Date"),
+                'default': purchase_invoice.posting_date
+            }
+        });
+        posting_date_link_field.refresh();
+        posting_date_link_field.set_value(purchase_invoice.posting_date);
     }
+
 }
