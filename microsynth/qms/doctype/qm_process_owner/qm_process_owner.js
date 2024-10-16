6e672458e2cc42585fd73ea4dd280ec173133583
Jens Petermann <jens.petermann@microsynth.ch>
2024-10-16 10:43:01 +0200

    New DocType to store process owners

---
+++ microsynth/qms/doctype/qm_process_owner/qm_process_owner.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Microsynth, libracore and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('QM Process Owner', {
+	// refresh: function(frm) {
+
+	// }
+});
