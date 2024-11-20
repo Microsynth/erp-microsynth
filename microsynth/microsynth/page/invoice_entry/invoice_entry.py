1f7328708234693ce793f2d91c1cf6d2eeee8ffe
Jens Petermann <jens.petermann@microsynth.ch>
2024-11-20 14:10:43 +0100

    Invoice Entry: Add net total and reorder fields

--- microsynth/microsynth/page/invoice_entry/invoice_entry.py
+++ microsynth/microsynth/page/invoice_entry/invoice_entry.py
@@ -27,6 +27,7 @@ def get_purchase_invoice_drafts():
             `tabPurchase Invoice`.`posting_date`,
             `tabPurchase Invoice`.`due_date`,
             `tabPurchase Invoice`.`net_total`,
+            `tabPurchase Invoice`.`total`,
             `tabPurchase Invoice`.`grand_total`,
             `tabPurchase Invoice`.`taxes_and_charges`,
             `tabPurchase Invoice`.`total_taxes_and_charges`,
