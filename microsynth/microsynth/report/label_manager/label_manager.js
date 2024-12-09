8f43e23eeae9bc224c602b6fdd0dfc6488e2f6f3
Jens Petermann <jens.petermann@microsynth.ch>
2024-12-09 14:40:58 +0100

    Label Manager: Notify user if status change failed

--- microsynth/microsynth/report/label_manager/label_manager.js
+++ microsynth/microsynth/report/label_manager/label_manager.js
@@ -216,10 +216,14 @@ frappe.query_reports["Label Manager"] = {
                                 },
                                 'freeze': true,
                                 'freeze_message': __('Setting ' + frappe.query_report.data.length + ' Labels to unused ...'),
-                                'callback': function(r)
-                                {
-                                    frappe.show_alert('Set Labels to unused');
-                                    frappe.click_button('Refresh');
+                                'callback': function(r) {
+									if (r.message.success) {
+                                        frappe.show_alert('Set Labels to unused');
+                                    	frappe.click_button('Refresh');
+                                    } else {
+                                        frappe.throw('Unable to set Labels to unused:<br>' + r.message.message + '<br><br>No Labels were set to unused.');
+                                    }
+                                    
                                 }
                             });
                         },
