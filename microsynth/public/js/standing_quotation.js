6d615ca81c419de5c310825d455c0f6de9f75401
Jens Petermann <jens.petermann@microsynth.ch>
2024-12-16 16:20:22 +0100

    Standing Quotation: Add hidden section and table "Shipping Items", implemented and commented out button "Populate Shipping Items" (Task #18485 KB ERP)

--- microsynth/public/js/standing_quotation.js
+++ microsynth/public/js/standing_quotation.js
@@ -27,6 +27,13 @@ frappe.ui.form.on('Standing Quotation', {
                 }
             }
         }
+
+        // add button "Populate Shipping Items"
+        // if ((!frm.doc.__islocal) && (frm.doc.docstatus === 0)) {
+        //     frm.add_custom_button(__("Populate Shipping Items"), function() {
+        //         populate_shipping_items();
+        //     });
+        // }
         
         // allow force cancel
         if ((!frm.doc.__islocal) && (frm.doc.docstatus === 0)) {
@@ -36,3 +43,26 @@ frappe.ui.form.on('Standing Quotation', {
         }
     },
 });
+
+
+function populate_shipping_items() {
+    frappe.call({
+        'method': "microsynth.microsynth.webshop.get_shipping_items",
+        'args': {
+            "customer_id": cur_frm.doc.customer
+        },
+        'callback': function(response) {
+            var shipping_items = response.message.shipping_items;
+            for (var i = 0; i < shipping_items.length; i++) {
+                var child = cur_frm.add_child('shipping_items');
+                frappe.model.set_value(child.doctype, child.name, 'item', shipping_items[i].item);
+                frappe.model.set_value(child.doctype, child.name, 'item_name', shipping_items[i].item_name);
+                frappe.model.set_value(child.doctype, child.name, 'qty', shipping_items[i].qty);
+                frappe.model.set_value(child.doctype, child.name, 'rate', shipping_items[i].rate);
+                frappe.model.set_value(child.doctype, child.name, 'threshold', shipping_items[i].threshold);
+                frappe.model.set_value(child.doctype, child.name, 'preferred_express', shipping_items[i].preferred_express);
+            }
+            cur_frm.refresh_field('shipping_items');
+        }
+    });
+}
