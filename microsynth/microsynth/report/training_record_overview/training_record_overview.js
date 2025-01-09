e976343442929466556434a72e2071e89cd23d5b
Jens Petermann <jens.petermann@microsynth.ch>
2025-01-09 16:49:46 +0100

    Delete empty report "Training Record Overview" since it is redundant to the already existing report "Training Confirmations"

--- microsynth/microsynth/report/training_record_overview/training_record_overview.js
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2024, Microsynth
-// For license information, please see license.txt
-/* eslint-disable */
-
-frappe.query_reports["Training Record Overview"] = {
-	"filters": [
-
-	]
-};
--- microsynth/microsynth/report/training_record_overview/training_record_overview.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, Microsynth
-# For license information, please see license.txt
-
-from __future__ import unicode_literals
-# import frappe
-
-def execute(filters=None):
-	columns, data = [], []
-	return columns, data
