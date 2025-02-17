f6dc9784216acc63118445fa9e9ab7331525df65
Jens Petermann <jens.petermann@microsynth.ch>
2025-02-17 12:39:25 +0100

    First Draft of Label Usage report (#19302 KB ERP)

--- microsynth/microsynth/report/label_usage/label_usage.js
+++ microsynth/microsynth/report/label_usage/label_usage.js
@@ -4,6 +4,32 @@
 
 frappe.query_reports["Label Usage"] = {
 	"filters": [
-
-	]
+		{
+            "fieldname": "from_barcode",
+            "label": __("From Barcode"),
+            "fieldtype": "Data",
+			"reqd": 1,
+            "on_change": function() {
+                if ((frappe.query_report.get_filter_value("from_barcode")) && (!frappe.query_report.get_filter_value("to_barcode"))) {
+                    frappe.query_report.set_filter_value("to_barcode", frappe.query_report.get_filter_value("from_barcode"));
+                }
+                frappe.query_report.refresh();
+            }
+        },
+        {
+            "fieldname": "to_barcode",
+            "label": __("To Barcode"),
+            "fieldtype": "Data",
+			"reqd": 1,
+            "on_change": function() {
+                if ((frappe.query_report.get_filter_value("to_barcode")) && (!frappe.query_report.get_filter_value("from_barcode"))) {
+                    frappe.query_report.set_filter_value("from_barcode", frappe.query_report.get_filter_value("to_barcode"));
+                }
+                frappe.query_report.refresh();
+            }
+        }
+	],
+    "onload": (report) => {
+        hide_chart_buttons();
+	}
 };
--- microsynth/microsynth/report/label_usage/label_usage.py
+++ microsynth/microsynth/report/label_usage/label_usage.py
@@ -2,8 +2,99 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
+from frappe import _
+import re
+
+
+def get_columns():
+    return [
+        {"label": _("Sales Order"), "fieldname": "name", "fieldtype": "Link", "options": "Sales Order", "width": 125},
+        {"label": _("Date"), "fieldname": "date", "fieldtype": "Date", "width": 75},
+        {"label": _("Customer"), "fieldname": "customer", "fieldtype": "Link", "options": "Customer", "width": 80},
+        {"label": _("Customer name"), "fieldname": "customer_name", "fieldtype": "Data", "width": 250},
+        {"label": _("Web Order ID"), "fieldname": "web_order_id", "fieldtype": "Data", "width": 95},
+        {"label": _("Sample count"), "fieldname": "sample_count", "fieldtype": "Integer", "width": 100},
+        # control fields:
+        {"label": _("Total"), "fieldname": "total", "fieldtype": "Currency", "options": "currency", "width": 95},
+        {"label": _("Status"), "fieldname": "status", "fieldtype": "Data", "width": 120},
+        {"label": _("Product Type"), "fieldname": "product_type", "fieldtype": "Data", "width": 110},
+        {"label": _("Company"), "fieldname": "company", "fieldtype": "Data", "width": 155},
+        #{"label": _("Punchout"), "fieldname": "is_punchout", "fieldtype": "Check", "width": 75},
+        #{"label": _("Hold Order"), "fieldname": "hold_order", "fieldtype": "Check", "width": 80},
+        #{"label": _("Hold Inv."), "fieldname": "hold_invoice", "fieldtype": "Check", "width": 70},
+        {"label": _("Creator"), "fieldname": "owner", "fieldtype": "Link", "options": "User", "width": 220},
+        {"label": _("Item"), "fieldname": "item", "fieldtype": "Link", "options": "Item", "width": 50}
+    ]
+
+
+def get_data(filters):
+    if not filters:
+        return []
+    conditions = ""
+
+    if filters.get('from_barcode') and filters.get('to_barcode'):
+        if filters.get('from_barcode').isnumeric() and filters.get('to_barcode').isnumeric():
+            from_barcode = filters.get('from_barcode')
+            to_barcode = filters.get('to_barcode')
+            if len(from_barcode) != len(to_barcode):
+                frappe.throw("From Barcode and To Barcode need to have the same length. Please use leading zeros if necessary.")
+            barcode_list = ','.join(f'"{i:0{len(to_barcode)}d}"' for i in range(int(from_barcode), int(to_barcode) + 1))
+            conditions += f"AND `tabSample`.`sequencing_label_id` IN ({barcode_list})"
+        else:
+            from_prefix = ''.join([i for i in filters.get('from_barcode') if not i.isdigit()])
+            to_prefix = ''.join([i for i in filters.get('to_barcode') if not i.isdigit()])
+            if from_prefix != to_prefix:
+                frappe.throw("From Barcode and To Barcode need to have the same Prefix.")
+            from_barcode = re.sub("[^0-9]", "", filters.get('from_barcode'))
+            to_barcode = re.sub("[^0-9]", "", filters.get('to_barcode'))
+            if len(from_barcode) != len(to_barcode):
+                frappe.throw("From Barcode and To Barcode need to have the same length.")
+            barcode_list = ','.join(f'"{to_prefix}{i:0{len(to_barcode)}d}"' for i in range(int(from_barcode), int(to_barcode) + 1))
+            conditions += f"AND `tabSample`.`sequencing_label_id` IN ({barcode_list})"
+    elif (filters.get('from_barcode') or filters.get('to_barcode')) and not conditions:
+        return []
+    elif filters.get('from_barcode') or filters.get('to_barcode'):
+        return []
+    
+    sql_query = f"""
+        SELECT `tabSales Order`.`name`,
+            `tabSales Order`.`transaction_date` AS `date`,
+            ROUND(`tabSales Order`.`total`, 2) AS `total`,
+            `tabSales Order`.`currency`,
+            `tabSales Order`.`customer`,
+            `tabSales Order`.`customer_name`,
+            `tabSales Order`.`web_order_id`,
+            `tabSales Order`.`status`,
+            `tabSales Order`.`product_type`,
+            (SELECT COUNT(`tabSample Link`.`name`)
+                FROM `tabSample Link`
+                WHERE `tabSample Link`.`parent` = `tabSales Order`.`name`
+            ) AS `sample_count`,
+            `tabSales Order`.`company`,
+            `tabSales Order`.`is_punchout`,
+            `tabSales Order`.`hold_order`,
+            `tabSales Order`.`hold_invoice`,
+            `tabSales Order`.`owner`,
+            (SELECT DISTINCT `tabSales Order Item`.`item_code`
+                FROM `tabSales Order Item`
+                WHERE `tabSales Order Item`.`parent` = `tabSales Order`.`name`
+                LIMIT 1
+            ) AS `item`
+        FROM `tabSales Order`
+        LEFT JOIN `tabSample Link` ON `tabSample Link`.`parent` = `tabSales Order`.`name`
+        LEFT JOIN `tabSample` ON `tabSample Link`.`sample` = `tabSample`.`name`
+        LEFT JOIN `tabSales Order Item` ON `tabSales Order Item`.`parent` = `tabSales Order`.`name`
+        WHERE `tabSales Order`.`docstatus` = 1
+            {conditions}
+        GROUP BY `tabSales Order`.`name`
+        ORDER BY `tabSales Order`.`transaction_date` DESC;
+        """
+    data = frappe.db.sql(sql_query, as_dict=True)
+    return data
+
 
 def execute(filters=None):
-	columns, data = [], []
-	return columns, data
+    columns = get_columns()
+    data = get_data(filters)
+    return columns, data
