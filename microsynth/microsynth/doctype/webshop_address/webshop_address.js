1a862145cacb4e0a73b2e7513d5b5cfef3a16a5a
Rolf Suter <rolf.suter@gmx.net>
2025-03-13 16:59:48 +0100

    Add Webshop Address

---
+++ microsynth/microsynth/doctype/webshop_address/webshop_address.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Microsynth, libracore and contributors and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Webshop Address', {
+	// refresh: function(frm) {
+
+	// }
+});
