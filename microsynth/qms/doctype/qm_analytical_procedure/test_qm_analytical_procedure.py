470646b8f7b7b64873d2c332e1a2c52df4ec90d7
Jens Petermann <jens.petermann@microsynth.ch>
2025-03-26 15:38:41 +0100

    First Draft of QM Analytical Procedure

---
+++ microsynth/qms/doctype/qm_analytical_procedure/test_qm_analytical_procedure.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2025, Microsynth, libracore and contributors and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+# import frappe
+import unittest
+
+class TestQMAnalyticalProcedure(unittest.TestCase):
+	pass
