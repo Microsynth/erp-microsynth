fee37fcbb18f1d0fae5283e6c15e2124acb26fa5
Jens Petermann <jens.petermann@microsynth.ch>
2025-03-31 15:31:47 +0200

    QMAP: Hide GMP section if Regulatory Classification is ISO 17025 and vice versa

--- microsynth/qms/doctype/qm_analytical_procedure/qm_analytical_procedure.js
+++ microsynth/qms/doctype/qm_analytical_procedure/qm_analytical_procedure.js
@@ -1,8 +1,23 @@
-// Copyright (c) 2025, Microsynth, libracore and contributors and contributors
+// Copyright (c) 2025, Microsynth
 // For license information, please see license.txt
 
 frappe.ui.form.on('QM Analytical Procedure', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		if (frm.doc.regulatory_classification == 'GMP') {
+			cur_frm.set_df_property('iso_17025_section', 'hidden', true);
+			cur_frm.set_df_property('gmp_assays_section', 'hidden', false);
+		} else if (frm.doc.regulatory_classification == 'ISO 17025') {
+			cur_frm.set_df_property('gmp_assays_section', 'hidden', true);
+			cur_frm.set_df_property('iso_17025_section', 'hidden', false);
+		}
+	},
+    regulatory_classification: function(frm) {
+        if (frm.doc.regulatory_classification == 'GMP') {
+			cur_frm.set_df_property('iso_17025_section', 'hidden', true);
+			cur_frm.set_df_property('gmp_assays_section', 'hidden', false);
+		} else if (frm.doc.regulatory_classification == 'ISO 17025') {
+			cur_frm.set_df_property('gmp_assays_section', 'hidden', true);
+			cur_frm.set_df_property('iso_17025_section', 'hidden', false);
+		}
+    }
 });
