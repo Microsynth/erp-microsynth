2b9d149589cca93da442b8012a9c4962326de3ae
Jens Petermann <jens.petermann@microsynth.ch>
2025-04-02 16:46:46 +0200

    QM Study: Enforce that completion date is set when submitting

--- microsynth/qms/doctype/qm_study/qm_study.js
+++ microsynth/qms/doctype/qm_study/qm_study.js
@@ -4,5 +4,11 @@
 frappe.ui.form.on('QM Study', {
 	// refresh: function(frm) {
 
-	// }
+	// },
+    before_submit(frm) {
+        if (!frm.doc.completion_date) {
+			frappe.msgprint("Please set the Completion Date.");
+			frappe.validated = false;
+		}
+    }
 });
