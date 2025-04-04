198d913e9c9f5f4b47eb6469da968a597ec0eb3c
Jens Petermann <jens.petermann@microsynth.ch>
2025-04-04 17:28:04 +0200

    QMAP: Set created_on and created_by on_submit

--- microsynth/qms/doctype/qm_analytical_procedure/qm_analytical_procedure.py
+++ microsynth/qms/doctype/qm_analytical_procedure/qm_analytical_procedure.py
@@ -3,8 +3,14 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
+from datetime import datetime
+
 
 class QMAnalyticalProcedure(Document):
-	pass
+    def on_submit(self):
+        self.created_by = frappe.session.user
+        self.created_on = datetime.today()
+        self.save()
+        frappe.db.commit()
