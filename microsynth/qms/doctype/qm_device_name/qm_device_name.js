bd2a2db0238681dc8ff4a13684564b2f47777657
Jens Petermann <jens.petermann@microsynth.ch>
2025-04-08 13:57:26 +0200

    Rename QM Device Type to QM Device Name

--- microsynth/qms/doctype/qm_device_name/qm_device_name.js
+++ microsynth/qms/doctype/qm_device_name/qm_device_name.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2025, Microsynth, libracore and contributors and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('QM Device Type', {
+frappe.ui.form.on('QM Device Name', {
 	// refresh: function(frm) {
 
 	// }
