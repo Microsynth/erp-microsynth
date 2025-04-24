5c4d7f8904734205370657e79e5b3011e49bec55
Rolf Suter <rolf.suter@gmx.net>
2025-04-24 15:54:01 +0200

    Delete Webshop Addresses that was substituted with Webshop Address previously.

--- microsynth/microsynth/doctype/webshop_addresses/webshop_addresses.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, Microsynth, libracore and contributors and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Webshop Addresses', {
-	// refresh: function(frm) {
-
-	// }
-});
