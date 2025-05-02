d62088ce87eadb3150fa7d2134b888aa2d47f3fe
Jens Petermann <jens.petermann@microsynth.ch>
2025-05-02 13:44:36 +0200

    New, still empty Report "Test Methods 17025" (Task #20251 KB ERP)

---
+++ microsynth/qms/report/test_methods_17025/test_methods_17025.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2025, Microsynth
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Test Methods 17025"] = {
+	"filters": [
+
+	]
+};
