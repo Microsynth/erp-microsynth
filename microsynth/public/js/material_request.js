ebb9074c2cec14d4763354a7f3b7d63c6c5aab3d
Jens Petermann <jens.petermann@microsynth.ch>
2025-05-16 12:08:48 +0200

    Material Request: Only propose Purchase Items with Item Group "Purchasing" (Task #18975)

--- microsynth/public/js/material_request.js
+++ microsynth/public/js/material_request.js
@@ -4,6 +4,18 @@ frappe.ui.form.on('Material Request', {
         if (frm.doc.__islocal) {
             prepare_naming_series(frm);             // common function
         }
+
+	    setTimeout(function () {
+            cur_frm.fields_dict.items.grid.get_field('item_code').get_query =  
+                function() {
+                    return {
+                        filters: {
+                            "is_purchase_item": 1,
+                            "item_group": "Purchasing"
+                        }
+                    };
+                };
+	    }, 1000);
         
         hide_in_words();
     },
