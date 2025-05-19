316dd628ce8b7f012d704a6f215b6c8b13d5be28
Jens Petermann <jens.petermann@microsynth.ch>
2025-05-19 17:19:47 +0200

    Open Material Requests: Prepare button "Create Purchase Order"

--- microsynth/microsynth/report/open_material_requests/open_material_requests.js
+++ microsynth/microsynth/report/open_material_requests/open_material_requests.js
@@ -9,6 +9,42 @@ frappe.query_reports["Open Material Requests"] = {
             label: __("Supplier"),
             fieldtype: "Link",
             options: "Supplier"
+        },
+        {
+            fieldname: "company",
+            label: __("Company"),
+            fieldtype: "Link",
+            options: "Company",
+            default: "Microsynth AG"
         }
-	]
+	],
+    "onload": (report) => {
+        hide_chart_buttons();
+
+        report.page.add_inner_button( __("Create Purchase Order"), function() {
+            create_purchase_order(report.get_values());
+        }).addClass("btn-primary");
+    }
 };
+
+function create_purchase_order(filters) {
+    if (!filters.supplier) {
+        frappe.msgprint( __("Please set the Supplier filter"), __("Validation") );
+    } else {
+        frappe.show_alert( __("Not yet implemented.") );
+        return;
+        frappe.call({
+            'method': "microsynth.microsynth.purchasing.create_po_from_open_mr",
+            'args':{
+                'filters': filters
+            },
+            'callback': function(r) {
+                if (r.message) {
+                    frappe.set_route("Form", "Purchase Order", r.message);
+                } else {
+                    frappe.show_alert("Internal Error");
+                }
+            }
+        });
+    }    
+}
