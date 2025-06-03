27ef6a0b961835349c768ce79a9b370448cd85f7
Jens Petermann <jens.petermann@microsynth.ch>
2025-06-03 14:01:45 +0200

    Price List: Show Reference Price List and buttons only if it is a Selling Price List

--- microsynth/public/js/price_list.js
+++ microsynth/public/js/price_list.js
@@ -1,6 +1,6 @@
 frappe.ui.form.on('Price List', {
     refresh(frm) {
-        if (!frm.doc.__islocal) {
+        if (!frm.doc.__islocal && frm.doc.selling) {
             // link to pricing configurator
             frm.add_custom_button(__("Price List"), function() {
                 frappe.set_route("query-report", "Pricing Configurator", {'price_list': frm.doc.name});
