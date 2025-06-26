98e7e40d2bf267b46d79bf2a5f2760514c9947c6
Jens Petermann <jens.petermann@microsynth.ch>
2025-06-26 11:15:55 +0200

    Implemented new Report "Purchasing Price History" (Task #20854 KB ERP)

--- microsynth/microsynth/report/purchasing_price_history/purchasing_price_history.js
+++ microsynth/microsynth/report/purchasing_price_history/purchasing_price_history.js
@@ -2,8 +2,40 @@
 // For license information, please see license.txt
 /* eslint-disable */
 
-frappe.query_reports["Purchasing Price History"] = {
-	"filters": [
 
-	]
+frappe.query_reports["Purchasing Price History"] = {
+    filters: [
+        {
+            fieldname: "item_code",
+            label: __("Item"),
+            fieldtype: "Link",
+            options: "Item",
+            get_query: function () {
+                return {
+                    filters: {
+                        'is_purchase_item': 1
+                    }
+                };
+            }
+        },
+        {
+            fieldname: "supplier",
+            label: __("Supplier"),
+            fieldtype: "Link",
+            options: "Supplier"
+        },
+        {
+            fieldname: "from_date",
+            label: __("From Date"),
+            fieldtype: "Date"
+        },
+        {
+            fieldname: "to_date",
+            label: __("To Date"),
+            fieldtype: "Date"
+        }
+    ],
+    "onload": (report) => {
+        hide_chart_buttons();
+    }
 };
--- microsynth/microsynth/report/purchasing_price_history/purchasing_price_history.py
+++ microsynth/microsynth/report/purchasing_price_history/purchasing_price_history.py
@@ -2,8 +2,63 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
+from frappe import _
+
+
+def get_columns():
+    return [
+        {"label": _("Date"), "fieldname": "posting_date", "fieldtype": "Date", "width": 85},
+        {"label": _("Item"), "fieldname": "item_code", "fieldtype": "Link", "options": "Item", "width": 320},
+        #{"label": _("Item Name"), "fieldname": "item_name", "fieldtype": "Data", "width": 180},
+        {"label": _("Supplier"), "fieldname": "supplier", "fieldtype": "Link", "options": "Supplier", "width": 70},
+        {"label": _("Supplier Name"), "fieldname": "supplier_name", "fieldtype": "Data", "width": 320},
+        {"label": _("Quantity"), "fieldname": "qty", "fieldtype": "Float", "width": 80},
+		{"label": _("UOM"), "fieldname": "uom", "fieldtype": "Link", "options": "UOM", "width": 90},
+        {"label": _("Rate"), "fieldname": "rate", "fieldtype": "Currency", "options": "currency", "width": 110},
+        {"label": _("Amount"), "fieldname": "amount", "fieldtype": "Currency", "options": "currency", "width": 120},
+		{"label": _("Invoice"), "fieldname": "invoice", "fieldtype": "Link", "options": "Purchase Invoice", "width": 90},
+        {"label": _("Currency"), "fieldname": "currency", "fieldtype": "Link", "options": "Currency", "width": 80}
+    ]
+
+
+def get_data(filters):
+    conditions = ""
+    if filters.get("item_code"):
+        conditions += " AND `tabPurchase Invoice Item`.`item_code` = %(item_code)s"
+    if filters.get("supplier"):
+        conditions += " AND `tabPurchase Invoice`.`supplier` = %(supplier)s"
+    if filters.get("from_date"):
+        conditions += " AND `tabPurchase Invoice`.`posting_date` >= %(from_date)s"
+    if filters.get("to_date"):
+        conditions += " AND `tabPurchase Invoice`.`posting_date` <= %(to_date)s"
+
+    return frappe.db.sql("""
+        SELECT
+            `tabPurchase Invoice`.`posting_date`,
+            `tabPurchase Invoice Item`.`item_code`,
+            `tabPurchase Invoice Item`.`item_name`,
+            `tabPurchase Invoice Item`.`uom`,
+            `tabPurchase Invoice`.`supplier`,
+            `tabPurchase Invoice`.`supplier_name`,
+            `tabPurchase Invoice`.`name` AS `invoice`,
+            `tabPurchase Invoice Item`.`qty`,
+            `tabPurchase Invoice Item`.`rate`,
+            `tabPurchase Invoice Item`.`amount`,
+            `tabPurchase Invoice`.`currency`
+        FROM
+            `tabPurchase Invoice`,
+            `tabPurchase Invoice Item`
+        WHERE
+            `tabPurchase Invoice`.`name` = `tabPurchase Invoice Item`.`parent`
+            AND `tabPurchase Invoice`.`docstatus` = 1
+            {conditions}
+        ORDER BY
+            `tabPurchase Invoice`.`posting_date` DESC;
+    """.format(conditions=conditions), filters, as_dict=True)
+
 
 def execute(filters=None):
-	columns, data = [], []
-	return columns, data
+    columns = get_columns()
+    data = get_data(filters)
+    return columns, data
