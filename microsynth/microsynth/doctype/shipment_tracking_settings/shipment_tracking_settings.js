bbab9d3d04e017cd4429918faebccb307f0b600e
Jens Petermann <jens.petermann@microsynth.ch>
2025-06-27 17:56:19 +0200

    New settings DocType to store UPS Tracking API credentials

---
+++ microsynth/microsynth/doctype/shipment_tracking_settings/shipment_tracking_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Microsynth
+// For license information, please see license.txt
+
+frappe.ui.form.on('Shipment Tracking Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
