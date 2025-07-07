7aeef7a4c56f8b2a4b86e6509e60b876a606fd5a
Jens Petermann <jens.petermann@microsynth.ch>
2025-07-07 11:49:09 +0200

    Recreate first QM Device draft due to NoneType issue (do not name a field "process")

--- microsynth/qms/doctype/qm_device/qm_device.js
+++ microsynth/qms/doctype/qm_device/qm_device.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2025, Microsynth
+// Copyright (c) 2025, Microsynth, libracore and contributors and contributors
 // For license information, please see license.txt
 
 frappe.ui.form.on('QM Device', {
