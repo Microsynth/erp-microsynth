a9df685b1389450256753545dc8f3aec24aec26d
Jens Petermann <jens.petermann@microsynth.ch>
2025-07-11 12:20:34 +0200

    Item Request: Implement reject button and functionality

--- microsynth/microsynth/doctype/item_request/item_request.js
+++ microsynth/microsynth/doctype/item_request/item_request.js
@@ -2,7 +2,35 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Item Request', {
-	// refresh: function(frm) {
-
-	// }
+    refresh: function(frm) {
+        // Show Reject button only for Purchase Manager and Purchase User
+        if (frappe.user.has_role('Purchase Manager') || frappe.user.has_role('Purchase User')) {
+            if (frm.doc.docstatus === 1 && frm.doc.status !== "Rejected") {
+                frm.add_custom_button(__('Reject'), function () {
+                    frappe.prompt([
+                        {
+                            'label': 'Reject Reason',
+                            'fieldname': 'reject_reason',
+                            'fieldtype': 'Small Text',
+                            'reqd': 1
+                        }
+                    ], function(values) {
+                        frappe.call({
+                            'method': "microsynth.microsynth.doctype.item_request.item_request.reject_item_request",
+                            'args': {
+                                'item_request': frm.doc.name,
+                                'reject_reason': values.reject_reason || ''
+                            },
+                            'callback': function(r) {
+                                if (!r.exc) {
+                                    frappe.msgprint(__("Item Request rejected"));
+                                    frm.reload_doc();
+                                }
+                            }
+                        });
+                    }, __('Reject Item Request'), __('Reject'));
+                }).addClass('btn-danger');
+            }
+        }
+    }
 });
--- microsynth/microsynth/doctype/item_request/item_request.py
+++ microsynth/microsynth/doctype/item_request/item_request.py
@@ -3,8 +3,30 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.desk.form.assign_to import add
+from microsynth.microsynth.utils import user_has_role
+
 
 class ItemRequest(Document):
 	pass
+
+
+@frappe.whitelist()
+def reject_item_request(item_request, reject_reason=None):
+    user = frappe.session.user
+    if not (user_has_role(user, 'Purchase Manager') or user_has_role(user, 'Purchase User')):
+        frappe.throw("You are not permitted to reject this request.")
+    doc = frappe.get_doc("Item Request", item_request)
+    doc.status = "Rejected"
+    doc.reject_message = reject_reason or "No reason provided."
+    doc.save()
+    note = f"Your Item Request '{item_request}' was rejected by {user}: {reject_reason or 'No reason provided.'}"
+    add({
+        'doctype': "Item Request",
+        'name': item_request,
+        'assign_to': doc.owner,
+        'description': note,
+        'notify': True
+    })
