80bc40a2a43f58f912b8721a3e1330e3e82e25a1
Jens Petermann <jens.petermann@microsynth.ch>
2025-07-24 08:36:08 +0200

    Modify button "Render & Download" to ask for a Contact, set a default to make testing easier (Task #21171 KB ERP)

--- microsynth/microsynth/doctype/price_list_print_template/price_list_print_template.js
+++ microsynth/microsynth/doctype/price_list_print_template/price_list_print_template.js
@@ -2,15 +2,35 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Price List Print Template', {
-	refresh: function(frm) {
-		frm.add_custom_button(__("Render & Download"), function() {
-			var w = window.open(
-				frappe.urllib.get_full_url("/api/method/microsynth.microsynth.webshop.get_price_list_doc"
-						+ "?contact=245438")
-			);
-			if (!w) {
-				frappe.msgprint(__("Please enable pop-ups")); return;
-			}
-		});
-	}
+    refresh: function(frm) {
+        frm.add_custom_button(__("Render & Download"), function() {
+            const d = new frappe.ui.Dialog({
+                'title': __("Select Contact"),
+                'fields': [
+                    {
+                        label: __("Contact"),
+                        fieldname: "contact",
+                        fieldtype: "Link",
+                        options: "Contact",
+                        reqd: 1,
+                        default: "222010"
+                    }
+                ],
+                'primary_action_label': __("Download"),
+                primary_action(values) {
+                    const encodedContact = encodeURIComponent(values.contact);
+                    const url = frappe.urllib.get_full_url(
+                        "/api/method/microsynth.microsynth.webshop.get_price_list_doc?contact=" + encodedContact
+                    );
+                    const w = window.open(url);
+                    if (!w) {
+                        frappe.msgprint(__("Please enable pop-ups"));
+                    }
+                    d.hide();
+                }
+            });
+
+            d.show();
+        });
+    }
 });
