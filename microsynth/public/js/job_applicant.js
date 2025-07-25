ccc426efd75c68ba87c9b2efc5b6197ce0d8f84c
Jens Petermann <jens.petermann@microsynth.ch>
2025-07-25 17:49:39 +0200

    Job Applicant: Add and populate a read-only address_display field

--- microsynth/public/js/job_applicant.js
+++ microsynth/public/js/job_applicant.js
@@ -6,5 +6,20 @@ frappe.ui.form.on('Job Applicant', {
         if (frm.doc.last_name) {spacer = " ";}
         // set full name
         cur_frm.set_value("applicant_name", (first_name + spacer + last_name));
+    },
+    address: function(frm) {
+        if (frm.doc.address) {
+            frappe.db.get_doc('Address', frm.doc.address)
+                .then(address => {
+                    const display = `
+${address.address_line1 || ''}
+${address.pincode || ''} ${address.city || ''}
+${address.country || ''}
+                    `.trim();
+                    frm.set_value("address_display", display);
+                });
+        } else {
+            frm.set_value("address_display", '');
+        }
     }
 })
