2303e56be71524ac260cc76b953f4b73ca9fc549
Jens Petermann <jens.petermann@microsynth.ch>
2025-07-28 09:30:09 +0200

    New, still empty Report "Job Applicant Overview"

---
+++ microsynth/microsynth/report/job_applicant_overview/job_applicant_overview.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2025, Microsynth
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Job Applicant Overview"] = {
+	"filters": [
+
+	]
+};
