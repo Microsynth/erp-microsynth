c93fbd39ffe0510de9b11a2cb34dd67e38346b4a
Jens Petermann <jens.petermann@microsynth.ch>
2025-09-03 16:53:20 +0200

    Start first draft of QM Logbook Entry

---
+++ microsynth/qms/doctype/qm_logbook_entry/qm_logbook_entry.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Microsynth, libracore and contributors and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('QM Logbook Entry', {
+	// refresh: function(frm) {
+
+	// }
+});
