ae223ac0639d70461d8cae07dbc8818b9c971e9f
Jens Petermann <jens.petermann@microsynth.ch>
2025-12-08 17:42:53 +0100

    Supplier Items report: Add get_queries for Item ID and Supplier filters

--- microsynth/microsynth/report/supplier_items/supplier_items.js
+++ microsynth/microsynth/report/supplier_items/supplier_items.js
@@ -9,7 +9,16 @@ frappe.query_reports["Supplier Items"] = {
             "fieldname": "item_id",
             "label": __("Item ID"),
             "fieldtype": "Link",
-            "options": "Item"
+            "options": "Item",
+            "get_query": function() {
+                return {
+                    "filters": {
+                        "item_group": "Purchasing",
+                        "is_purchase_item": 1,
+                        "disabled": 0
+                    }
+                };
+            }
         },
         {
             "fieldname": "item_name",
@@ -20,7 +29,14 @@ frappe.query_reports["Supplier Items"] = {
             "fieldname": "supplier",
             "label": __("Supplier"),
             "fieldtype": "Link",
-            "options": "Supplier"
+            "options": "Supplier",
+            "get_query": function() {
+                return {
+                    "filters": {
+                        "disabled": 0
+                    }
+                };
+            }
         },
         {
             "fieldname": "supplier_part_no",
