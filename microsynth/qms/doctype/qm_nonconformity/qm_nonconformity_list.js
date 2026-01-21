a4c5a05116dc566546e81c99f3d6e0b9643c43a5
Jens Petermann <jens.petermann@microsynth.ch>
2026-01-21 13:58:37 +0100

    QM Nonconformity: Add Clear button to the List view

--- microsynth/qms/doctype/qm_nonconformity/qm_nonconformity_list.js
+++ microsynth/qms/doctype/qm_nonconformity/qm_nonconformity_list.js
@@ -17,5 +17,8 @@ frappe.listview_settings['QM Nonconformity'] = {
             "Cancelled": "red"
         };
         return [__(doc.status), status_color[doc.status], "status,=,"+doc.status];
+    },
+    onload: function(doc) {
+        add_clear_button();
     }
 };
