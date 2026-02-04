2f239458c9471b814eebdf1f20459787362e5e12
Rolf Suter <rolf.suter@gmx.net>
2026-02-04 10:02:14 +0100

    Remove trailing whitespace

--- microsynth/patches/v0_207_0/v0_207_0.py
+++ microsynth/patches/v0_207_0/v0_207_0.py
@@ -2,16 +2,16 @@ import frappe
 
 def execute():
     print("Check and extend Webshop Service InvoiceByDefaultCompany...")
-    
+
     frappe.reload_doc("Microsynth", "doctype", "Webshop Service")
-    
+
     if not frappe.db.exists("Webshop Service", "InvoiceByDefaultCompany"):
         new_service = frappe.get_doc({
             'doctype': "Webshop Service",
             'service_name': "InvoiceByDefaultCompany"
         })
         new_service.insert()
-        
+
         frappe.db.commit()
-    
+
     return
