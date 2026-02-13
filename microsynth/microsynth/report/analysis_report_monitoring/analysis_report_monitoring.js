a6537f5ab4d6412228b8bb5dd74032ad658b0982
Jens Petermann <jens.petermann@microsynth.ch>
2026-02-13 15:19:32 +0100

    New, still empty Report "Analysis Report Monitoring" (Task #23207 KB ERP)

---
+++ microsynth/microsynth/report/analysis_report_monitoring/analysis_report_monitoring.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2026, Microsynth
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Analysis Report Monitoring"] = {
+	"filters": [
+
+	]
+};
