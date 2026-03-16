557b8b34b25636d0f621c22f122231759b645f4d
Jens Petermann <jens.petermann@microsynth.ch>
2026-03-16 15:27:34 +0100

    QM Instrument: Set fields as read-only for unauthorized users and add button to block instrument

--- microsynth/qms/doctype/qm_instrument/qm_instrument.js
+++ microsynth/qms/doctype/qm_instrument/qm_instrument.js
@@ -1,8 +1,30 @@
-// Copyright (c) 2025, Microsynth, libracore and contributors and contributors
+// Copyright (c) 2025, Microsynth, libracore and contributors
 // For license information, please see license.txt
 
 frappe.ui.form.on('QM Instrument', {
-	// refresh: function(frm) {
+	refresh: function(frm) {
+		// Set all fields to read-only if user is not QAU, System Manager, instrument_manager, or deputy_instrument_manager
+        const user = frappe.session.user;
+        const isQAU = frappe.user_roles.includes('QAU');
+        const isManager = frm.doc.instrument_manager === user;
+        const isDeputy = frm.doc.deputy_instrument_manager === user;
 
-	// }
+        if (!frm.doc.__islocal && !isQAU && !isManager && !isDeputy && !frappe.user_roles.includes('System Manager')) {
+            frm.fields.forEach(field => {
+				if (field.df && field.df.fieldname) {
+					console.log(`Setting field ${field.df.fieldname} to read-only`);
+					frm.set_df_property(field.df.fieldname, 'read_only', 1);
+				}
+            });
+        }
+
+		if (!frm.doc.__islocal && frm.doc.status !== 'Out of order') {
+			frm.add_custom_button(__('Block Instrument'), function() {
+				frm.set_value('status', 'Out of order');
+				frm.save();
+				frm.refresh();
+				frappe.msgprint(__('Instrument has been blocked.'));
+			}).addClass("btn-danger");
+		}
+	}
 });
