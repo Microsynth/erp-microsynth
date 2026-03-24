9b5c1f6f873f026015a3e53e8f73271868680434
Jens Petermann <jens.petermann@microsynth.ch>
2026-03-24 08:49:30 +0100

    Item Request: Update status options and enhance list view indicators

--- microsynth/microsynth/doctype/item_request/item_request_list.js
+++ microsynth/microsynth/doctype/item_request/item_request_list.js
@@ -1,4 +1,20 @@
 frappe.listview_settings['Item Request'] = {
+    get_indicator: function(doc) {
+        if(doc.status=="Pending") {
+            return [__("Pending"), "orange", "status,=,Pending"];
+        } else if(doc.status=="Rejected") {
+            return [__("Rejected"), "red", "status,=,Rejected"];
+        } else if(doc.docstatus==2) {
+            return [__("Cancelled"), "red", "docstatus,=,2"];
+        } else if(doc.status=="Done") {
+            return [__("Done"), "green", "status,=,Done"];
+        } else if(doc.docstatus==0) {
+            return [__("Draft"), "red", "docstatus,=,0"];
+        }
+    },
+    onload: function(listview) {
+        add_clear_button();
+    },
     refresh: function(listview) {
         if (frappe.user.has_role('System Manager')) return;
         // Hide the New button
