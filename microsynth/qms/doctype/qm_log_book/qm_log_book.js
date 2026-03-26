8444cfc7c093ee95aa31ef1edccf44aded3b53ec
Jens Petermann <jens.petermann@microsynth.ch>
2026-03-26 10:01:40 +0100

    Add QM Log Book DocType

---
+++ microsynth/qms/doctype/qm_log_book/qm_log_book.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Microsynth
+// For license information, please see license.txt
+
+frappe.ui.form.on('QM Log Book', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ microsynth/qms/doctype/qm_log_book/test_qm_log_book.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2026, Microsynth
+# See license.txt
+from __future__ import unicode_literals
+
+# import frappe
+import unittest
+
+class TestQMLogBook(unittest.TestCase):
+	pass
