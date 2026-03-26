9764a0c0192b8722048598c33fba75914b8f67d9
Jens Petermann <jens.petermann@microsynth.ch>
2026-03-26 10:44:28 +0100

    Link Log Book Entry to QM Instrument

--- microsynth/qms/doctype/qm_instrument/qm_instrument.js
+++ microsynth/qms/doctype/qm_instrument/qm_instrument.js
@@ -2,8 +2,8 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('QM Instrument', {
-	refresh: function(frm) {
-		// Set all fields to read-only if user is not QAU, System Manager, instrument_manager, or deputy_instrument_manager
+    refresh: function(frm) {
+        // Set all fields to read-only if user is not QAU, System Manager, instrument_manager, or deputy_instrument_manager
         const user = frappe.session.user;
         const isQAU = frappe.user_roles.includes('QAU');
         const isManager = frm.doc.instrument_manager === user;
@@ -11,20 +11,30 @@ frappe.ui.form.on('QM Instrument', {
 
         if (!frm.doc.__islocal && !isQAU && !isManager && !isDeputy && !frappe.user_roles.includes('System Manager')) {
             frm.fields.forEach(field => {
-				if (field.df && field.df.fieldname) {
-					console.log(`Setting field ${field.df.fieldname} to read-only`);
-					frm.set_df_property(field.df.fieldname, 'read_only', 1);
-				}
+                if (field.df && field.df.fieldname) {
+                    console.log(`Setting field ${field.df.fieldname} to read-only`);
+                    frm.set_df_property(field.df.fieldname, 'read_only', 1);
+                }
             });
         }
 
-		if (!frm.doc.__islocal && frm.doc.status !== 'Out of order') {
-			frm.add_custom_button(__('Block Instrument'), function() {
-				frm.set_value('status', 'Out of order');
-				frm.save();
-				frm.refresh();
-				frappe.msgprint(__('Instrument has been blocked.'));
-			}).addClass("btn-danger");
-		}
-	}
+        if (!frm.doc.__islocal && frm.doc.status !== 'Out of order') {
+            frm.add_custom_button(__('Block Instrument'), function() {
+                frm.set_value('status', 'Out of order');
+                frm.save();
+                frm.refresh();
+                frappe.msgprint(__('Instrument has been blocked.'));
+            }).addClass("btn-danger");
+        }
+
+        // Add Button Create > Log Book Entry
+        if (!frm.doc.__islocal) {
+            frm.add_custom_button(__('Log Book Entry'), function() {
+                frappe.new_doc('QM Log Book', {
+                    document_type: frm.doc.doctype,
+                    document_name: frm.doc.name
+                });
+            }, __('Create'));
+        }
+    }
 });
--- microsynth/qms/doctype/qm_log_book/qm_log_book.js
+++ microsynth/qms/doctype/qm_log_book/qm_log_book.js
@@ -2,7 +2,13 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('QM Log Book', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		if (frm.doc.__islocal && !frm.doc.document_name) {
+			frm.dashboard.add_comment(__("Please create this Log Book Entry from the QM Instrument."), "red", true);
+		}
+		if (!frm.doc.__islocal && frm.doc.document_type && frm.doc.document_name) {
+			// do not allow to relink the log book entry to another document after it has been created
+			frm.set_df_property("document_name", "read_only", true);
+		}
+	}
 });
