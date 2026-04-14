5e103ada1789c3bd874e68897ec7b33dc56f39bf
Jens Petermann <jens.petermann@microsynth.ch>
2026-04-14 12:25:07 +0200

    Log Book Review Required report: adjust columns and add 'Created by' column

--- microsynth/qms/report/log_book_review_required/log_book_review_required.py
+++ microsynth/qms/report/log_book_review_required/log_book_review_required.py
@@ -11,22 +11,23 @@ def get_columns():
         # QM Log Book fields
         {"label": "Log Book Entry", "fieldname": "name", "fieldtype": "Link", "options": "QM Log Book", "width": 105},
         #{"label": "Status", "fieldname": "status", "fieldtype": "Data", "width": 100},
-        {"label": "Entry Type", "fieldname": "entry_type", "fieldtype": "Data", "width": 140},
+        {"label": "Entry Type", "fieldname": "entry_type", "fieldtype": "Data", "width": 135},
         {"label": "Date", "fieldname": "date", "fieldtype": "Date", "width": 80},
-        {"label": "Description", "fieldname": "description", "fieldtype": "Text", "width": 200},
+		{"label": "Created by", "fieldname": "log_book_creator", "fieldtype": "Link", "options": "User", "width": 150},
+        {"label": "Description", "fieldname": "description", "fieldtype": "Text", "width": 250},
         #{"label": "Costs", "fieldname": "costs", "fieldtype": "Link", "options": "Purchase Invoice", "width": 150},
-        {"label": "Instrument ID", "fieldname": "document_name", "fieldtype": "Link", "options": "QM Instrument", "width": 90},
+        {"label": "Instrument ID", "fieldname": "document_name", "fieldtype": "Link", "options": "QM Instrument", "width": 95},
 
         # QM Instrument fields
-        {"label": "Instrument Name", "fieldname": "instrument_name", "fieldtype": "Data", "width": 160},
+        {"label": "Instrument Name", "fieldname": "instrument_name", "fieldtype": "Data", "width": 200},
         {"label": "Instrument Status", "fieldname": "instrument_status", "fieldtype": "Data", "width": 120},
-        {"label": "Instrument Class", "fieldname": "instrument_class", "fieldtype": "Data", "width": 115},
-        {"label": "Regulatory Classification", "fieldname": "regulatory_classification", "fieldtype": "Data", "width": 155},
-        {"label": "Site", "fieldname": "site", "fieldtype": "Data", "width": 90},
-        {"label": "Manufacturer", "fieldname": "manufacturer", "fieldtype": "Data", "width": 140},
+        {"label": "Instrument Class", "fieldname": "instrument_class", "fieldtype": "Data", "width": 180},
+        {"label": "Regulatory Classification", "fieldname": "regulatory_classification", "fieldtype": "Data", "width": 150},
+        {"label": "Site", "fieldname": "site", "fieldtype": "Data", "width": 85},
+        #{"label": "Manufacturer", "fieldname": "manufacturer", "fieldtype": "Data", "width": 140},
         {"label": "Serial No", "fieldname": "serial_no", "fieldtype": "Data", "width": 100},
         #{"label": "Supplier", "fieldname": "supplier", "fieldtype": "Link", "options": "Supplier", "width": 75},
-        {"label": "Supplier Name", "fieldname": "supplier_name", "fieldtype": "Data", "width": 200},
+        #{"label": "Supplier Name", "fieldname": "supplier_name", "fieldtype": "Data", "width": 200},
     ]
 
 
@@ -57,6 +58,7 @@ def get_data(filters):
             `tabQM Log Book`.`status`,
             `tabQM Log Book`.`entry_type`,
             `tabQM Log Book`.`date`,
+            `tabQM Log Book`.`owner` AS `log_book_creator`,
             `tabQM Log Book`.`document_name`,
             `tabQM Log Book`.`description`,
             `tabQM Log Book`.`costs`,
