21c4f17022e3b40c20a1ee59073a68f4bbd97106
Jens Petermann <jens.petermann@microsynth.ch>
2026-04-30 15:59:23 +0200

    Prepare import functionality for QM Log Book entries in Instrument Compliance Due report

--- microsynth/qms/report/instrument_compliance_due/instrument_compliance_due.js
+++ microsynth/qms/report/instrument_compliance_due/instrument_compliance_due.js
@@ -13,5 +13,21 @@ frappe.query_reports["Instrument Compliance Due"] = {
     ],
 	"onload": (report) => {
 		hide_chart_buttons();
+        // Show "Import Certificates" button if User has role QAU calling a backend method to import QM Log Book Entries from ERP-Share
+        if (report.page && !report.page.btn_import_certificates && frappe.user.has_role("QAU")) {
+            report.page.btn_import_certificates = report.page.add_inner_button(__('Import Certificates'), function() {
+                frappe.msgprint(__('Not yet implemented.'));
+                // frappe.call({
+                //     'method': "microsynth.qms.doctype.qm_log_book.qm_log_book.import_log_book_entries",
+                //     'args': { 'verbose': true },
+                //     'freeze': true,
+                //     'freeze_message': __('Importing certificates...'),
+                //     'callback': function(r) {
+                //         frappe.msgprint(__('Import completed'));
+                //         report.refresh();
+                //     }
+                // });
+            });
+        }
 	}
 };
