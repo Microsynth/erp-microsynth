a5c5b673f3a53474cfd20bb2e0159081412388bf
Rolf Suter <rolf.suter@gmx.net>
2026-05-27 10:17:25 +0200

    Restructure Webshop Label API: Move get_registered_label_ranges and required depencency functions to new API file.

--- microsynth/microsynth/webshop.py
+++ microsynth/microsynth/webshop.py
@@ -2109,94 +2109,14 @@ def get_label_ranges():
     return get_label_ranges()
 
 
-def is_next_barcode(first_barcode, second_barcode):
-    """
-    Check if second_barcode follows immediatly after first_barcode
-    """
-    try:
-        first_int = int(first_barcode)
-        second_int = int(second_barcode)
-        return first_int + 1 == second_int
-    except Exception:
-        try:
-            # compile a regex
-            cre = re.compile("([a-zA-Z]+)([0-9]+)")
-            # match it to group text and numbers separately into a tuple
-            first_split = cre.match(first_barcode).groups()
-            second_split = cre.match(second_barcode).groups()
-            # check if label prefixes are identical
-            if first_split[0] == second_split[0]:
-                return int(first_split[1]) + 1 == int(second_split[1])
-            else:
-                return False
-        except Exception:
-            return False
-
-
-def partition_into_ranges(sequencing_labels):
-    """
-    Takes a list of dictionaries of labels sorted by barcode ascending and returns a list of dictionary of barcode ranges.
-    """
-    ranges = []
-    if len(sequencing_labels) < 1:
-        return ranges
-    current_range_barcode = sequencing_labels[0]['barcode']
-    barcode_range = {
-        'registered_to': sequencing_labels[0]['registered_to'],
-        'item': sequencing_labels[0]['item'],
-        'barcode_start_range': current_range_barcode,
-        'barcode_end_range': current_range_barcode
-    }
-    for i, label in enumerate(sequencing_labels):
-        if i == 0:
-            continue  # do not consider the first label a second time
-        if label['registered_to'] != barcode_range['registered_to'] or label['item'] != barcode_range['item'] or not is_next_barcode(current_range_barcode, label['barcode']):
-            # finish current barcode_range
-            barcode_range['barcode_end_range'] = current_range_barcode
-            ranges.append(barcode_range)
-            # start a new barcode_range
-            barcode_range = {
-                'registered_to': label['registered_to'],
-                'item': label['item'],
-                'barcode_start_range': label['barcode'],
-                'barcode_end_range': label['barcode']
-            }
-        current_range_barcode = label['barcode']
-    # finish last barcode_range
-    barcode_range['barcode_end_range'] = current_range_barcode
-    if not barcode_range in ranges:
-        # add last barcode_range
-        ranges.append(barcode_range)
-    return ranges
-
-
 @frappe.whitelist()
 def get_registered_label_ranges(contacts):
     """
     bench execute microsynth.microsynth.webshop.get_registered_label_ranges --kwargs "{'contacts': ['215856', '237365', 'invalid_contact']}"
     """
-    # Check parameter
-    if not contacts or len(contacts) == 0:
-        return {'success': False, 'message': "Failed to get registered label ranges.", 'internal_message': "Please provide at least one Contact", 'ranges': None}
-    try:
-        sql_query = """
-            SELECT `item`,
-                `label_id` AS `barcode`,
-                `registered_to`
-            FROM `tabSequencing Label`
-            WHERE `status` = 'unused'
-                AND `registered_to` IN ({})
-            ORDER BY `label_id` ASC
-            ;""".format(','.join(['%s'] * len(contacts)))
-        sequencing_labels = frappe.db.sql(sql_query, contacts, as_dict=True)
-        if len(sequencing_labels) == 0:
-            return {'success': True, 'message': 'OK', 'internal_message': 'No sequencing labels found.', 'ranges': []}
-        ranges = partition_into_ranges(sequencing_labels)
-        return {'success': True, 'message': 'OK', 'internal_message': 'OK', 'ranges': ranges}
-    except Exception as err:
-        msg = f"Error fetching registered label ranges for contacts {contacts}: {err}. Check ERP Error Log for details."
-        frappe.log_error(f"{msg}\n\n\n{traceback.format_exc()}", "webshop.get_registered_label_ranges")
-        return {'success': False, 'message': "Failed to get registered label ranges.", 'internal_message': msg, 'ranges': None}
+    from microsynth.microsynth.api.webshop.label import get_registered_label_ranges
+    #TODO: include a error log to notify about usage of this deprecated function once the webshop is updated
+    return get_registered_label_ranges(contacts)
 
 
 def check_label_range(item, prefix, first_int, second_int):
