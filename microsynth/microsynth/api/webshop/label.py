8767d05c4b27f0b070d6e5c5c4fbcff992479e8a
Rolf Suter <rolf.suter@gmx.net>
2026-05-27 08:42:52 +0200

    Restructure Webshop Label API: Move get_label_status to new API file.

--- microsynth/microsynth/api/webshop/label.py
+++ microsynth/microsynth/api/webshop/label.py
@@ -39,3 +39,77 @@ def get_unused_labels(contacts, items):
         msg = f"Error fetching unused labels for contacts {contacts} and items {items}: {err}. Check ERP Error Log for details."
         frappe.log_error(f"{msg}\n\n\n{traceback.format_exc()}", "webshop.get_unused_labels")
         return {'success': False, 'message': "Failed to get unused labels", 'internal_message': msg, 'labels': None}
+
+
+@frappe.whitelist()
+def get_label_status(labels):
+    """
+    Check uniqueness of label: throw an error if multiple Labels with the barcode are found
+
+    bench execute microsynth.microsynth.api.webshop.label.get_label_status --kwargs "{'labels': [{'item': '6030', 'barcode': 'MY00043'}, {'item': '6030', 'barcode': 'MY00047'}]}"
+    """
+    # Check parameter
+    if not labels or len(labels) == 0:
+        return {'success': True, 'messages': []}
+    try:
+        messages_to_return = []
+        for label in labels:
+            if not 'item' in label or not label['item'] or not 'barcode' in label or not label['barcode']:
+                messages_to_return.append({
+                    'query': label,
+                    'label': None,
+                    'message': "Failed to get label status",
+                    'internal_message': f"Label '{label['barcode']}' does not exist."  # Item and Barcode are both mandatory.
+                })
+                continue
+            if not frappe.db.exists("Item", label['item']):
+                messages_to_return.append({
+                    'query': label,
+                    'label': None,
+                    'message': "Failed to get label status",
+                    'internal_message': f"Label '{label['barcode']}' does not exist."  # f"The given Item '{label['item']}' does not exist in the ERP."
+                })
+                continue
+            item_condition = f"AND `item` = {label['item']}"
+            item_string = f" and Item Code {label['item']}"
+            sql_query = f"""
+                SELECT `item`,
+                    `label_id` AS `barcode`,
+                    `status`,
+                    `registered`,
+                    `contact`,
+                    `registered_to`
+                FROM `tabSequencing Label`
+                WHERE `label_id` = '{label['barcode']}'
+                    {item_condition}
+                ;"""
+            sequencing_labels = frappe.db.sql(sql_query, as_dict=True)
+            if len(sequencing_labels) > 1:
+                frappe.log_error(f"Found {len(sequencing_labels)} labels for the given barcode {label['barcode']}{item_string}.", "webshop.get_label_status")
+                messages_to_return.append({
+                    'query': label,
+                    'label': None,
+                    'internal_message': f"Found {len(sequencing_labels)} labels for the given barcode {label['barcode']}{item_string}.",
+                    'message': f"Label '{label['barcode']}' is not valid. Please contact the Microsynth support."
+                })
+                continue
+            elif len(sequencing_labels) == 0:
+                #frappe.log_error(f"Found no label for the given barcode {label['barcode']}{item_string}.", "webshop.get_label_status")
+                messages_to_return.append({
+                    'query': label,
+                    'label': None,
+                    'internal_message': f"Label '{label['barcode']}' does not exist.",
+                    'message': "Error getting label status"
+                })
+                continue
+            else:
+                messages_to_return.append({
+                    'query': label,
+                    'label': sequencing_labels[0],
+                    'message': "OK",
+                    'internal_message': "OK"
+                })
+        return {'success': True, 'messages': messages_to_return}
+    except Exception as err:
+        frappe.log_error(f"{labels=}\n{err}", "webshop.get_label_status")
+        return {'success': False, 'messages': [{'query': None, 'label': None, 'message': err}]}
--- microsynth/microsynth/webshop.py
+++ microsynth/microsynth/webshop.py
@@ -2092,71 +2092,9 @@ def get_label_status(labels):
 
     bench execute microsynth.microsynth.webshop.get_label_status --kwargs "{'labels': [{'item': '6030', 'barcode': 'MY00043'}, {'item': '6030', 'barcode': 'MY00047'}]}"
     """
-    # Check parameter
-    if not labels or len(labels) == 0:
-        return {'success': True, 'messages': []}
-    try:
-        messages_to_return = []
-        for label in labels:
-            if not 'item' in label or not label['item'] or not 'barcode' in label or not label['barcode']:
-                messages_to_return.append({
-                    'query': label,
-                    'label': None,
-                    'message': "Failed to get label status",
-                    'internal_message': f"Label '{label['barcode']}' does not exist."  # Item and Barcode are both mandatory.
-                })
-                continue
-            if not frappe.db.exists("Item", label['item']):
-                messages_to_return.append({
-                    'query': label,
-                    'label': None,
-                    'message': "Failed to get label status",
-                    'internal_message': f"Label '{label['barcode']}' does not exist."  # f"The given Item '{label['item']}' does not exist in the ERP."
-                })
-                continue
-            item_condition = f"AND `item` = {label['item']}"
-            item_string = f" and Item Code {label['item']}"
-            sql_query = f"""
-                SELECT `item`,
-                    `label_id` AS `barcode`,
-                    `status`,
-                    `registered`,
-                    `contact`,
-                    `registered_to`
-                FROM `tabSequencing Label`
-                WHERE `label_id` = '{label['barcode']}'
-                    {item_condition}
-                ;"""
-            sequencing_labels = frappe.db.sql(sql_query, as_dict=True)
-            if len(sequencing_labels) > 1:
-                frappe.log_error(f"Found {len(sequencing_labels)} labels for the given barcode {label['barcode']}{item_string}.", "webshop.get_label_status")
-                messages_to_return.append({
-                    'query': label,
-                    'label': None,
-                    'internal_message': f"Found {len(sequencing_labels)} labels for the given barcode {label['barcode']}{item_string}.",
-                    'message': f"Label '{label['barcode']}' is not valid. Please contact the Microsynth support."
-                })
-                continue
-            elif len(sequencing_labels) == 0:
-                #frappe.log_error(f"Found no label for the given barcode {label['barcode']}{item_string}.", "webshop.get_label_status")
-                messages_to_return.append({
-                    'query': label,
-                    'label': None,
-                    'internal_message': f"Label '{label['barcode']}' does not exist.",
-                    'message': "Error getting label status"
-                })
-                continue
-            else:
-                messages_to_return.append({
-                    'query': label,
-                    'label': sequencing_labels[0],
-                    'message': "OK",
-                    'internal_message': "OK"
-                })
-        return {'success': True, 'messages': messages_to_return}
-    except Exception as err:
-        frappe.log_error(f"{labels=}\n{err}", "webshop.get_label_status")
-        return {'success': False, 'messages': [{'query': None, 'label': None, 'message': err}]}
+    from microsynth.microsynth.api.webshop.label import get_label_status
+    #TODO: include a error log to notify about usage of this deprecated function once the webshop is updated
+    return get_label_status(labels)
 
 
 @frappe.whitelist()
