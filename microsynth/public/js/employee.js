510ecd5690a02b0835e65683e83e03086f4d74a7
Jens Petermann <jens.petermann@microsynth.ch>
2026-06-23 17:17:51 +0200

    Allow to create an Employee Onboarding from an Employee (Task #25422 KB ERP)

---
+++ microsynth/public/js/employee.js
@@ -0,0 +1,8 @@
+try {
+    cur_frm.dashboard.add_transactions([
+        {
+            'label': 'Lifecycle',
+            'items': ['Employee Onboarding']
+        }
+    ]);
+} catch { /* do nothing */ }
